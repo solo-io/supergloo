369cb15dd71125a1cdcd411f7858aef43b048024
Rick Ducott <rick.ducott@solo.io>
2018-11-16 08:16:24 -0500

    Update script comment

--- hack/install/helm/install-helm.sh
+++ hack/install/helm/install-helm.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-# Expected to be run from a machine with kubectl and helm installed
+# Expected to be run from a machine with kubectl and helm 2 installed
 # This script will initialize helm on kubernetes
 
 kubectl apply -f helm-service-account.yaml
 
-# Required for helm 2, this installs tiller on kubernetes in the "kube-system" namespace
+# This installs tiller on kubernetes in the "kube-system" namespace
 helm init --service-account tiller --upgrade
