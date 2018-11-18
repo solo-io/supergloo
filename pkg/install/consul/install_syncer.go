1d9af77ec1cf31b366793bbe1718f5321ac1328b
Rick Ducott <rick.ducott@solo.io>
2018-11-17 21:24:30 -0500

    Update, refactor, make install resilient; tests are solid

--- pkg/install/consul/install_syncer.go
+++ pkg/install/consul/install_syncer.go
@@ -5,62 +5,147 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/pkg/errors"
+	kubecore "k8s.io/api/core/v1"
+	kuberbac "k8s.io/api/rbac/v1"
+	kubemeta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
 	"github.com/solo-io/supergloo/pkg/api/v1"
 	helmlib "k8s.io/helm/pkg/helm"
 
 	"github.com/solo-io/supergloo/pkg/install/helm"
 )
 
-type ConsulInstallSyncer struct{}
+const (
+	crbName          = "consul-crb"
+	defaultNamespace = "consul"
+)
 
-func (c *ConsulInstallSyncer) Sync(_ context.Context, snap *v1.InstallSnapshot) error {
+type ConsulInstallSyncer struct {
+	Kube *kubernetes.Clientset
+}
 
+func (c *ConsulInstallSyncer) Sync(ctx context.Context, snap *v1.InstallSnapshot) error {
+	for _, install := range snap.Installs.List() {
+		c.SyncInstall(ctx, install)
+	}
+	return nil
+}
+
+func (c *ConsulInstallSyncer) SyncInstall(_ context.Context, install *v1.Install) error {
 	// See hack/install/consul/install-on... for steps
 	// 1. Create a namespace / project for consul (this step should go away and we use provided namespace)
 	// 2. Set up ClusterRoleBinding for consul in that namespace (not currently done)
 	// 3. Install Consul via helm chart
 	// 4. Fix incorrect configuration -> webhook service looking for wrong adapter config name (this should move into an override)
-
-	for _, install := range snap.Installs.List() {
-		if install.Consul != nil {
-
-			// TODO: Allow specifying namespace in the proto
-			namespace := "consul"
-			// TODO: Create namespace
-
-			updatedOverrides := overridesYaml
-			if install.Encryption != nil {
-				strBool := strconv.FormatBool(install.Encryption.TlsEnabled)
-				updatedOverrides = strings.Replace(overridesYaml, "@@MTLS_ENABLED@@", strBool, -1)
-			}
-
-			// helm install
-			helmClient, err := helm.GetHelmClient()
-			if err != nil {
-				helm.Teardown() // just in case
-				return err
-			}
-
-			if err != nil {
-				helm.Teardown() // just in case
-				return err
-			}
-
-			_, err = helmClient.InstallRelease(
-				install.Consul.Path,
-				namespace,
-				helmlib.ValueOverrides([]byte(updatedOverrides)))
-			helm.Teardown()
-			if err != nil {
-				return err
-			}
-
-			// TODO: Fix incorrect webhook configuration
-
-			// Create Mesh CRD
-		}
+	if install.Consul == nil {
+		return nil
 	}
+
+	installNamespace := getInstallNamespace(install.Consul)
+
+	err := c.createNamespaceIfNotExist(installNamespace) // extract to CRD
+	if err != nil {
+		return errors.Wrap(err, "Error setting up namespace")
+	}
+
+	err = c.createCrbIfNotExist(installNamespace)
+	if err != nil {
+		return errors.Wrap(err, "Error setting up CRB")
+	}
+
+	err = helmInstall(install.Encryption, install.Consul, installNamespace)
+	if err != nil {
+		return errors.Wrap(err, "Error installing Consul helm chart")
+	}
+
+	// TODO: Fix incorrect webhook configuration
+	// TODO: Create Mesh CRD
+
 	return nil
+}
+
+func getInstallNamespace(consul *v1.ConsulInstall) string {
+	installNamespace := defaultNamespace
+	if consul.Namespace != "" {
+		installNamespace = consul.Namespace
+	}
+	return installNamespace
+}
+
+func (c *ConsulInstallSyncer) createNamespaceIfNotExist(namespaceName string) error {
+	_, err := c.Kube.CoreV1().Namespaces().Get(namespaceName, kubemeta.GetOptions{})
+	if err == nil {
+		// Namespace already exists
+		return nil
+	}
+	_, err = c.Kube.CoreV1().Namespaces().Create(getNamespace(namespaceName))
+	return err
+}
+
+func getNamespace(namespaceName string) *kubecore.Namespace {
+	return &kubecore.Namespace{
+		ObjectMeta: kubemeta.ObjectMeta{
+			Name: namespaceName,
+		},
+	}
+}
+
+func (c *ConsulInstallSyncer) createCrbIfNotExist(namespaceName string) error {
+	_, err := c.Kube.RbacV1().ClusterRoleBindings().Get(crbName, kubemeta.GetOptions{})
+	if err == nil {
+		// crb already exists
+		return nil
+	}
+	_, err = c.Kube.RbacV1().ClusterRoleBindings().Create(getCrb(namespaceName))
+	return err
+}
+
+func getCrb(namespaceName string) *kuberbac.ClusterRoleBinding {
+	meta := kubemeta.ObjectMeta{
+		Name: "consul-crb",
+	}
+	subject := kuberbac.Subject{
+		Kind:      "ServiceAccount",
+		Namespace: namespaceName,
+		Name:      "default",
+	}
+	roleRef := kuberbac.RoleRef{
+		Kind:     "ClusterRole",
+		Name:     "cluster-admin",
+		APIGroup: "rbac.authorization.k8s.io",
+	}
+	return &kuberbac.ClusterRoleBinding{
+		ObjectMeta: meta,
+		Subjects:   []kuberbac.Subject{subject},
+		RoleRef:    roleRef,
+	}
+}
+
+func helmInstall(encryption *v1.Encryption, consul *v1.ConsulInstall, installNamespace string) error {
+	overrides := []byte(getOverrides(encryption))
+	// helm install
+	helmClient, err := helm.GetHelmClient()
+	if err != nil {
+		return err
+	}
+
+	_, err = helmClient.InstallRelease(
+		consul.Path,
+		installNamespace,
+		helmlib.ValueOverrides(overrides))
+	helm.Teardown()
+	return err
+}
+
+func getOverrides(encryption *v1.Encryption) string {
+	updatedOverrides := overridesYaml
+	if encryption != nil {
+		strBool := strconv.FormatBool(encryption.TlsEnabled)
+		updatedOverrides = strings.Replace(overridesYaml, "@@MTLS_ENABLED@@", strBool, -1)
+	}
+	return updatedOverrides
 }
 
 var overridesYaml = `
--- pkg/install/consul/install_syncer_test.go
+++ pkg/install/consul/install_syncer_test.go
@@ -5,7 +5,12 @@ import (
 	"os"
 	"path/filepath"
 
+	kubecore "k8s.io/api/core/v1"
+
+	kubemeta "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
 	. "github.com/onsi/ginkgo"
@@ -15,57 +20,89 @@ import (
 )
 
 var _ = Describe("ConsulInstallSyncer", func() {
+
+	namespace := "consul"
+
+	getKubeConfig := func() *rest.Config {
+		kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		Expect(err).NotTo(HaveOccurred())
+		return cfg
+	}
+
+	getKubeClient := func() *kubernetes.Clientset {
+		cfg := getKubeConfig()
+		client, err := kubernetes.NewForConfig(cfg)
+		Expect(err).NotTo(HaveOccurred())
+		return client
+	}
+
+	getSnapshot := func(mtls bool) *v1.InstallSnapshot {
+		return &v1.InstallSnapshot{
+			Installs: v1.InstallsByNamespace{
+				"not_used": v1.InstallList{
+					&v1.Install{
+						Consul: &v1.ConsulInstall{
+							Path:      "/Users/rick/.helm/cache/archive/v0.3.0.tar.gz",
+							Namespace: namespace,
+						},
+						Encryption: &v1.Encryption{
+							TlsEnabled: mtls,
+						},
+					},
+				},
+			},
+		}
+	}
+
+	terminateNamespaceBlocking := func() {
+		client := getKubeClient()
+		client.CoreV1().Namespaces().Delete(namespace, &kubemeta.DeleteOptions{})
+		Eventually(func() error {
+			_, err := client.CoreV1().Namespaces().Get(namespace, kubemeta.GetOptions{})
+			return err
+		}, "60s", "1s").ShouldNot(BeNil()) // will be non-nil when NS is gone
+	}
+
+	waitForAvailablePods := func() {
+		client := getKubeClient()
+		Eventually(func() bool {
+			podList, err := client.CoreV1().Pods(namespace).List(kubemeta.ListOptions{})
+			Expect(err).To(BeNil())
+			done := true
+			for _, pod := range podList.Items {
+				for _, condition := range pod.Status.Conditions {
+					if condition.Type == kubecore.PodReady && condition.Status != kubecore.ConditionTrue {
+						done = false
+					}
+				}
+			}
+			return done
+		}, "60s", "1s").Should(BeTrue())
+	}
+
+	AfterEach(func() {
+		terminateNamespaceBlocking()
+	})
+
 	It("Can install consul with mtls enabled", func() {
-		syncer := consul.ConsulInstallSyncer{}
-		var ctx context.Context
-		snap := v1.InstallSnapshot{
-			Installs: v1.InstallsByNamespace{
-				"not_used": v1.InstallList{
-					&v1.Install{
-						Consul: &v1.ConsulInstall{
-							Path: "/Users/rick/.helm/cache/archive/v0.3.0.tar.gz",
-						},
-						Encryption: &v1.Encryption{
-							TlsEnabled: true,
-						},
-					},
-				},
-			},
+		syncer := consul.ConsulInstallSyncer{
+			Kube: getKubeClient(),
 		}
-		err := syncer.Sync(ctx, &snap)
+		snap := getSnapshot(true)
+		err := syncer.Sync(context.TODO(), snap)
 		Expect(err).NotTo(HaveOccurred())
-
-		kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-		Expect(err).NotTo(HaveOccurred())
-		_, err = kubernetes.NewForConfig(cfg)
-		Expect(err).NotTo(HaveOccurred())
+		waitForAvailablePods()
 	})
 
-	FIt("Can install consul without mtls enabled", func() {
-		syncer := consul.ConsulInstallSyncer{}
-		var ctx context.Context
-		snap := v1.InstallSnapshot{
-			Installs: v1.InstallsByNamespace{
-				"not_used": v1.InstallList{
-					&v1.Install{
-						Consul: &v1.ConsulInstall{
-							Path: "/Users/rick/.helm/cache/archive/v0.3.0.tar.gz",
-						},
-						Encryption: &v1.Encryption{
-							TlsEnabled: false,
-						},
-					},
-				},
-			},
+	It("Can install consul without mtls enabled", func() {
+		syncer := consul.ConsulInstallSyncer{
+			Kube: getKubeClient(),
 		}
-		err := syncer.Sync(ctx, &snap)
+		snap := getSnapshot(false)
+		err := syncer.Sync(context.TODO(), snap)
 		Expect(err).NotTo(HaveOccurred())
-
-		kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-		Expect(err).NotTo(HaveOccurred())
-		_, err = kubernetes.NewForConfig(cfg)
-		Expect(err).NotTo(HaveOccurred())
+		waitForAvailablePods()
 	})
+
 })
