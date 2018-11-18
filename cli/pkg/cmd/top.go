6fd7734d76c47aab799be7ab25d3d670487eea9f
Mitch Kelley <mitch.kelley@solo.io>
2018-11-18 17:31:26 -0500

    add top-level cmds

--- cli/pkg/cmd/top.go
+++ cli/pkg/cmd/top.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/solo-io/supergloo/cli/pkg/cmd/install"
 	"github.com/solo-io/supergloo/cli/pkg/cmd/options"
 	"github.com/spf13/cobra"
@@ -23,7 +25,46 @@ func App(version string) *cobra.Command {
 	app.SuggestionsMinimumDistance = 1
 	app.AddCommand(
 		install.Cmd(&opts),
+		faultInjection(&opts),
+		loadBalancing(&opts),
+		retries(&opts),
 	)
 
 	return app
+}
+
+func faultInjection(opts *options.Options) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "fault-injection",
+		Short: `stress test your mesh with faults`,
+		Long:  `stress test your mesh with faults`,
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Println("this feature will be available in 2019")
+		},
+	}
+	return cmd
+}
+
+func loadBalancing(opts *options.Options) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "load-balancing",
+		Short: `specify traffic distribution`,
+		Long:  `specify traffic distribution`,
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Println("this feature will be available in 2019")
+		},
+	}
+	return cmd
+}
+
+func retries(opts *options.Options) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "retries",
+		Short: `configure retry parameters`,
+		Long:  `configure retry parameters`,
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Println("this feature will be available in 2019")
+		},
+	}
+	return cmd
 }
