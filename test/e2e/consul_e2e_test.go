fed89815055043b512569ca98150513a256849c9
Rick Ducott <rick.ducott@solo.io>
2018-11-19 09:08:28 -0500

    Start refactor of test helpers

--- test/e2e/consul_e2e_test.go
+++ test/e2e/consul_e2e_test.go
@@ -3,29 +3,16 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	gloo "github.com/solo-io/supergloo/pkg/api/external/gloo/v1"
+	"github.com/solo-io/supergloo/test/util"
 
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 
-	"github.com/hashicorp/consul/api"
-
-	"github.com/solo-io/solo-kit/pkg/api/v1/clients/factory"
-	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kube"
-	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
-	kubecore "k8s.io/api/core/v1"
-
-	kubemeta "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kube"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	"github.com/solo-io/supergloo/pkg/api/v1"
 	"github.com/solo-io/supergloo/pkg/install/consul"
 	consulSync "github.com/solo-io/supergloo/pkg/translator/consul"
@@ -47,20 +34,7 @@ var _ = Describe("ConsulInstallSyncer", func() {
 	meshName := "test-consul-mesh"
 	secretName := "test-tls-secret"
 	consulPort := 8500
-
-	getKubeConfig := func() *rest.Config {
-		kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-		Expect(err).NotTo(HaveOccurred())
-		return cfg
-	}
-
-	getKubeClient := func() *kubernetes.Clientset {
-		cfg := getKubeConfig()
-		client, err := kubernetes.NewForConfig(cfg)
-		Expect(err).NotTo(HaveOccurred())
-		return client
-	}
+	kubeCache := kube.NewKubeCache()
 
 	getSnapshot := func(mtls bool, secret *core.ResourceRef) *v1.InstallSnapshot {
 		return &v1.InstallSnapshot{
@@ -85,57 +59,6 @@ var _ = Describe("ConsulInstallSyncer", func() {
 		}
 	}
 
-	terminateNamespaceBlocking := func() {
-		client := getKubeClient()
-		client.CoreV1().Namespaces().Delete(installNamespace, &kubemeta.DeleteOptions{})
-		Eventually(func() error {
-			_, err := client.CoreV1().Namespaces().Get(installNamespace, kubemeta.GetOptions{})
-			return err
-		}, "60s", "1s").ShouldNot(BeNil()) // will be non-nil when NS is gone
-	}
-
-	waitForAvailablePods := func() {
-		client := getKubeClient()
-		Eventually(func() bool {
-			podList, err := client.CoreV1().Pods(installNamespace).List(kubemeta.ListOptions{})
-			Expect(err).To(BeNil())
-			done := true
-			for _, pod := range podList.Items {
-				for _, condition := range pod.Status.Conditions {
-					if condition.Type == kubecore.PodReady && condition.Status != kubecore.ConditionTrue {
-						done = false
-					}
-				}
-			}
-			return done
-		}, "60s", "1s").Should(BeTrue())
-	}
-
-	kubeCache := kube.NewKubeCache()
-
-	getMeshClient := func(restConfig *rest.Config) v1.MeshClient {
-		meshClient, err := v1.NewMeshClient(&factory.KubeResourceClientFactory{
-			Crd:         v1.MeshCrd,
-			Cfg:         restConfig,
-			SharedCache: kubeCache,
-		})
-		Expect(err).Should(BeNil())
-		err = meshClient.Register()
-		Expect(err).Should(BeNil())
-		return meshClient
-	}
-
-	deleteCrb := func() {
-		client := getKubeClient()
-		err := client.RbacV1().ClusterRoleBindings().Delete(consul.CrbName, &kubemeta.DeleteOptions{})
-		Expect(err).Should(BeNil())
-	}
-
-	deleteWebhookConfigIfExists := func() {
-		client := getKubeClient()
-		client.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Delete(consul.WebhookCfg, &kubemeta.DeleteOptions{})
-	}
-
 	getTranslatorSnapshot := func(mesh *v1.Mesh, secret *gloo.Secret) *v1.TranslatorSnapshot {
 		secrets := gloo.SecretsByNamespace{}
 		if secret != nil {
@@ -155,86 +78,21 @@ var _ = Describe("ConsulInstallSyncer", func() {
 		}
 	}
 
-	testKey := "-----BEGIN PRIVATE KEY-----\nMIG2AgEAMBAGByqGSM49AgEGBSuBBAAiBIGeMIGbAgEBBDBoI1sMdiOTvBBdjWlS\nZ8qwNuK9xV4yKuboLZ4Sx/OBfy1eKZocxTKvnjLrHUe139uhZANiAAQMTIR56O8U\nTIqf6uUHM4i9mZYLj152up7elS06Gi6lk7IeUQDHxP0NnOnbhC7rmtOV6myLNApL\nQ92kZKg7qa8q7OY/4w1QfC4ch7zZKxjNkSIiuAx7V/lzF6FYDcqT3js=\n-----END PRIVATE KEY-----"
-	testRoot := "-----BEGIN CERTIFICATE-----\nMIIB7jCCAXUCCQC2t6Lqc2xnXDAKBggqhkjOPQQDAjBhMQswCQYDVQQGEwJVUzEW\nMBQGA1UECAwNTWFzc2FjaHVzZXR0czESMBAGA1UEBwwJQ2FtYnJpZGdlMQwwCgYD\nVQQKDANPcmcxGDAWBgNVBAMMD3d3dy5leGFtcGxlLmNvbTAeFw0xODExMTgxMzQz\nMDJaFw0xOTExMTgxMzQzMDJaMGExCzAJBgNVBAYTAlVTMRYwFAYDVQQIDA1NYXNz\nYWNodXNldHRzMRIwEAYDVQQHDAlDYW1icmlkZ2UxDDAKBgNVBAoMA09yZzEYMBYG\nA1UEAwwPd3d3LmV4YW1wbGUuY29tMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEDEyE\neejvFEyKn+rlBzOIvZmWC49edrqe3pUtOhoupZOyHlEAx8T9DZzp24Qu65rTleps\nizQKS0PdpGSoO6mvKuzmP+MNUHwuHIe82SsYzZEiIrgMe1f5cxehWA3Kk947MAoG\nCCqGSM49BAMCA2cAMGQCMCytVFc8sBdbM7DaBCz0N2ptdb0T7LFFfxDTzn4gjiDq\nVCd/3dct21TUWsthKXF2VgIwXEMI5EQiJ5kjR/y1KNBC9b4wfDiKRvG33jYe9gn6\ntzXUS00SoqG9D27/7aK71/xv\n-----END CERTIFICATE-----"
-	testCertChain := ""
-
-	getSecretClient := func(kube *kubernetes.Clientset) gloo.SecretClient {
-		secretClient, err := gloo.NewSecretClient(&factory.KubeSecretClientFactory{
-			Clientset: kube,
-		})
-		Expect(err).Should(BeNil())
-		err = secretClient.Register()
-		Expect(err).Should(BeNil())
-		return secretClient
-	}
-
-	createSecret := func(secretClient gloo.SecretClient) (*gloo.Secret, *core.ResourceRef) {
-		tls := gloo.TlsSecret{
-			RootCa:     testRoot,
-			PrivateKey: testKey,
-			CertChain:  testCertChain,
-		}
-		tlsWrapper := gloo.Secret_Tls{
-			Tls: &tls,
-		}
-		secret := &gloo.Secret{
-			Metadata: core.Metadata{
-				Namespace: superglooNamespace,
-				Name:      secretName,
-			},
-			Kind: &tlsWrapper,
-		}
-		secretClient.Delete(superglooNamespace, secretName, clients.DeleteOpts{})
-		_, err := secretClient.Write(secret, clients.WriteOpts{})
-		Expect(err).NotTo(HaveOccurred())
-		ref := &core.ResourceRef{
-			Namespace: superglooNamespace,
-			Name:      secretName,
-		}
-		return secret, ref
-	}
-
-	getConsulServerPodName := func(client kubernetes.Interface) string {
-		podList, err := client.CoreV1().Pods(installNamespace).List(kubemeta.ListOptions{})
-		Expect(err).NotTo(HaveOccurred())
-		for _, pod := range podList.Items {
-			if strings.Contains(pod.Name, "consul-server-0") {
-				return pod.Name
-			}
-		}
-		// Should not have happened
-		Expect(false).To(BeTrue())
-		return ""
-	}
-
-	// New creates a new and initialized tunnel.
-	createConsulTunnel := func(client kubernetes.Interface) (*helmkube.Tunnel, error) {
-		podName := getConsulServerPodName(client)
-		t := helmkube.NewTunnel(client.CoreV1().RESTClient(), getKubeConfig(), installNamespace, podName, consulPort)
-		return t, t.ForwardPort()
-	}
-
-	checkCertUpdated := func(port int) {
-		config := &api.Config{
-			Address: fmt.Sprintf("127.0.0.1:%d", port),
-		}
-		client, err := api.NewClient(config)
-		Expect(err).NotTo(HaveOccurred())
-		var queryOpts api.QueryOptions
-		currentConfig, _, err := client.Connect().CAGetConfig(&queryOpts)
-		Expect(err).NotTo(HaveOccurred())
-
-		currentRoot := currentConfig.Config["RootCert"]
-		Expect(currentRoot).To(BeEquivalentTo(testRoot))
-	}
-
 	var tunnel *helmkube.Tunnel
 	var meshClient v1.MeshClient
 	var secretClient gloo.SecretClient
+	var installSyncer consul.ConsulInstallSyncer
+
+	BeforeEach(func() {
+		meshClient = util.GetMeshClient(kubeCache)
+		secretClient = util.GetSecretClient()
+		installSyncer = consul.ConsulInstallSyncer{
+			Kube:       util.GetKubeClient(),
+			MeshClient: meshClient,
+		}
+	})
 
 	AfterEach(func() {
-		fmt.Printf("Cleaning up after test")
 		// Delete secret
 		if tunnel != nil {
 			tunnel.Close()
@@ -247,38 +105,24 @@ var _ = Describe("ConsulInstallSyncer", func() {
 			secretClient.Delete(superglooNamespace, secretName, clients.DeleteOpts{})
 		}
 
-		deleteWebhookConfigIfExists()
-		deleteCrb()
-		terminateNamespaceBlocking()
+		util.DeleteWebhookConfigIfExists(consul.WebhookCfg)
+		util.DeleteCrb(consul.CrbName)
+		util.TerminateNamespaceBlocking(installNamespace)
 	})
 
 	It("Can install consul with mtls enabled", func() {
-		fmt.Print("Setting up clients\n")
-		kubeCfg := getKubeConfig()
-		secretClient = getSecretClient(getKubeClient())
-		secret, ref := createSecret(secretClient)
-		meshClient = getMeshClient(kubeCfg)
-
-		fmt.Printf("Install mesh\n")
-		installSyncer := consul.ConsulInstallSyncer{
-			Kube:       getKubeClient(),
-			MeshClient: meshClient,
-		}
+		secret, ref := util.CreateTestSecret(superglooNamespace, secretName)
 		snap := getSnapshot(true, ref)
 		err := installSyncer.Sync(context.TODO(), snap)
 		Expect(err).NotTo(HaveOccurred())
 
-		fmt.Printf("Wait for pods to be ready\n")
-		waitForAvailablePods()
-
-		fmt.Printf("Reading mesh object\n")
+		util.WaitForAvailablePods(installNamespace)
 		mesh, err := meshClient.Read(superglooNamespace, meshName, clients.ReadOpts{})
 		Expect(err).NotTo(HaveOccurred())
 
-		fmt.Printf("Opening consul tunnel\n")
-		tunnel, err = createConsulTunnel(getKubeClient())
+		tunnel, err = util.CreateConsulTunnel(installNamespace, consulPort)
 		Expect(err).NotTo(HaveOccurred())
-		fmt.Printf("Syncing mesh\n")
+
 		meshSyncer := consulSync.ConsulSyncer{
 			LocalPort: tunnel.Local,
 		}
@@ -287,19 +131,14 @@ var _ = Describe("ConsulInstallSyncer", func() {
 		Expect(err).NotTo(HaveOccurred())
 
 		fmt.Printf("Validating cert got updated\n")
-		checkCertUpdated(tunnel.Local)
+		util.CheckCertMatches(tunnel.Local, util.TestRoot)
 	})
 
 	It("Can install consul without mtls enabled", func() {
-		meshClient = getMeshClient(getKubeConfig())
-		installSyncer := consul.ConsulInstallSyncer{
-			Kube:       getKubeClient(),
-			MeshClient: meshClient,
-		}
 		snap := getSnapshot(false, nil)
 		err := installSyncer.Sync(context.TODO(), snap)
 		Expect(err).NotTo(HaveOccurred())
-		waitForAvailablePods()
+		util.WaitForAvailablePods(installNamespace)
 
 		mesh, err := meshClient.Read(superglooNamespace, meshName, clients.ReadOpts{})
 		Expect(err).NotTo(HaveOccurred())
