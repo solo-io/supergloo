97d5ab1358d7436ea6daae651323c2c0753413f6
Mitch Kelley <mitch.kelley@solo.io>
2018-11-19 21:27:34 -0500

    update to linkerd2

--- cli/pkg/cmd/install/install.go
+++ cli/pkg/cmd/install/install.go
@@ -20,7 +20,7 @@ func Cmd(opts *options.Options) *cobra.Command {
 	pflags := cmd.PersistentFlags()
 	// TODO(mitchdraft) - remove filename or apply it to something
 	pflags.StringVarP(&iop.Filename, "filename", "f", "", "filename to create resources from")
-	pflags.StringVarP(&iop.MeshType, "meshtype", "m", "", "mesh to install: istio, consul, linkerd")
+	pflags.StringVarP(&iop.MeshType, "meshtype", "m", "", "mesh to install: istio, consul, linkerd2")
 	pflags.StringVarP(&iop.Namespace, "namespace", "n", "", "namespace install mesh into")
 	pflags.BoolVar(&iop.Mtls, "mtls", false, "use MTLS")
 	pflags.StringVar(&iop.SecretRef.Name, "secret.name", "", "name of the MTLS secret")
@@ -44,7 +44,7 @@ func install(opts *options.Options) {
 	case "istio":
 		fmt.Println("istio TODO")
 		return
-	case "linkerd":
+	case "linkerd2":
 		fmt.Println("ld TODO")
 		return
 	default:
--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,6 +2,6 @@ package constants
 
 var (
 	SuperglooNamespace = "supergloo-system"
-	MeshOptions        = []string{"istio", "consul", "linkerd"}
+	MeshOptions        = []string{"istio", "consul", "linkerd2"}
 	ConsulInstallPath  = "https://github.com/hashicorp/consul-helm/archive/v0.3.0.tar.gz"
 )
