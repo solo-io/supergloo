631e2842fcedeb888fe8df7833360738ba8439b3
Rick Ducott <rick.ducott@solo.io>
2018-12-03 18:56:28 -0500

    Clean up

--- test/e2e/istio_e2e_test.go
+++ test/e2e/istio_e2e_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/solo-io/solo-kit/pkg/utils/contextutils"
@@ -120,8 +121,7 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 
 	BeforeEach(func() {
 		randStr := helpers.RandString(8)
-		//installNamespace = "istio-install-test-" + randStr
-		installNamespace = "istio-system"
+		installNamespace = "istio-install-test-" + randStr
 		meshName = "istio-mesh-test-" + randStr
 		secretName = "istio-secret-test-" + randStr
 		bookinfoNamespace = "istio-bookinfo-test-" + randStr
@@ -158,7 +158,7 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 		util.TerminateNamespace(bookinfoNamespace)
 	})
 
-	deployBookInfo := func() string {
+	deployBookInfoAndWaitForAvailable := func() string {
 		// create namespace for bookinfo
 		ns := &kubecore.Namespace{
 			ObjectMeta: kubemeta.ObjectMeta{
@@ -168,13 +168,18 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 				},
 			},
 		}
+		logger.Infof("Creating namespace %s", bookinfoNamespace)
 		util.GetKubeClient().CoreV1().Namespaces().Create(ns)
 
 		bookinfo := "https://raw.githubusercontent.com/istio/istio/4c0a001b5e542d43b4c66ae75c1f41f2c1ff183e/samples/bookinfo/platform/kube/bookinfo.yaml"
 		kubectlargs := []string{"apply", "-n", bookinfoNamespace, "-f", bookinfo}
 		cmd := exec.Command("kubectl", kubectlargs...)
+		logger.Infof("Deploying bookinfo")
 		err := cmd.Run()
 		Expect(err).NotTo(HaveOccurred())
+		logger.Infof("Waiting until bookinfo pods are ready")
+		Expect(util.WaitForAvailablePodsWithTimeout(bookinfoNamespace, "500s")).Should(BeEquivalentTo(6))
+		logger.Infof("Bookinfo pods are ready")
 		return bookinfoNamespace
 	}
 
@@ -244,6 +249,8 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 				kubectlArgs = append(kubectlArgs, "--key", "/etc/certs/key.pem", "--cert", "/etc/certs/cert-chain.pem", "--cacert", "/etc/certs/root-cert.pem", "-k")
 			}
 
+			argsString := strings.Join(kubectlArgs, " ")
+			logger.Infof("Curling productpage with command: kubectl %s\n", argsString)
 			cmd := exec.Command("kubectl", kubectlArgs...)
 			err = cmd.Run()
 			if err != nil {
@@ -276,9 +283,8 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 			Expect(cmd.Run()).To(BeNil())
 
 			// Shouldn't be able to plain text curl from inside the pod
-			deployBookInfo()
-			util.WaitForAvailablePodsWithTimeout(bookinfoNamespace, "500s")
-			Eventually(tlsCurlSucceeds, "60s", "1s").Should(BeTrue()) // can take awhile after pod starts up for this to work
+			deployBookInfoAndWaitForAvailable()
+			Eventually(tlsCurlSucceeds, "120s", "1s").Should(BeTrue()) // can take awhile after pod starts up for this to work
 			Expect(curlSucceeds(true)).To(BeFalse())
 		})
 
@@ -289,11 +295,10 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(util.WaitForAvailablePods(installNamespace)).To(BeEquivalentTo(9))
 
-			deployBookInfo()
-			Expect(util.WaitForAvailablePodsWithTimeout(bookinfoNamespace, "500s")).To(BeEquivalentTo(6))
+			deployBookInfoAndWaitForAvailable()
 
 			// plain text and tls curling should work from inside the pod
-			Eventually(plainTextCurlSucceeds, "60s", "1s").Should(BeTrue()) // can take awhile after pod starts up for this to work
+			Eventually(plainTextCurlSucceeds, "120s", "1s").Should(BeTrue()) // can take awhile after pod starts up for this to work
 			Expect(tlsCurlSucceeds()).To(BeTrue())
 
 			// turn on mtls
@@ -325,8 +330,7 @@ var _ = Describe("Istio Install and Encryption E2E", func() {
 			Expect(err).NotTo(HaveOccurred())
 			util.WaitForAvailablePodsWithTimeout(installNamespace, "300s")
 
-			deployBookInfo()
-			util.WaitForAvailablePodsWithTimeout(bookinfoNamespace, "500s")
+			deployBookInfoAndWaitForAvailable()
 
 			mesh, err := meshClient.Read(constants.SuperglooNamespace, meshName, clients.ReadOpts{})
 			Expect(err).NotTo(HaveOccurred())
