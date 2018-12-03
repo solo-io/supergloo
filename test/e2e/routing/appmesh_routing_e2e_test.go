b45683e0e1fd53b990e86edde3610709c1c6ee3d
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-12-03 15:07:16 -0500

    route tests fixes (still WIP); added a deep clean for aws mesh resources referring to services with FQDN so that envoy will route to them/

--- test/e2e/routing/appmesh_routing_e2e_test.go
+++ test/e2e/routing/appmesh_routing_e2e_test.go
@@ -1,9 +1,13 @@
 package routing
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
+	"runtime/debug"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -30,18 +34,28 @@ import (
 	"github.com/solo-io/supergloo/test/utils"
 )
 
-var _ = FDescribe("appmesh routing E2e", func() {
+var _ = Describe("appmesh routing E2e", func() {
 	var namespace string
 
 	BeforeEach(func() {
 		namespace = "appmesh-routing-test-" + helpers.RandString(8)
-		namespace = "supergloo-system"
-		return
 		err := testsetup.SetupKubeForTest(namespace)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	AfterEach(func() {
+
+		if false {
+			// wait for sig usr1
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGUSR1)
+			defer signal.Reset(syscall.SIGUSR1)
+			fmt.Println("We are here:")
+			debug.PrintStack()
+			fmt.Printf("Waiting for human intervention. to continue, run 'kill -SIGUSR1 %d'\n", os.Getpid())
+			<-c
+		}
+
 		gexec.TerminateAndWait(2 * time.Second)
 
 		sess, err := session.NewSession(aws.NewConfig().
@@ -51,35 +65,72 @@ var _ = FDescribe("appmesh routing E2e", func() {
 		list, err := appmeshClient.ListMeshes(&awsappmesh.ListMeshesInput{})
 		Expect(err).NotTo(HaveOccurred())
 		for _, mesh := range list.Meshes {
-			_, err := appmeshClient.DeleteMesh(&awsappmesh.DeleteMeshInput{
+
+			vnlist, err := appmeshClient.ListVirtualNodes(&awsappmesh.ListVirtualNodesInput{
+				MeshName: mesh.MeshName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			for _, vn := range vnlist.VirtualNodes {
+				_, err := appmeshClient.DeleteVirtualNode(&awsappmesh.DeleteVirtualNodeInput{
+					MeshName:        mesh.MeshName,
+					VirtualNodeName: vn.VirtualNodeName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+			}
+
+			vrlist, err := appmeshClient.ListVirtualRouters(&awsappmesh.ListVirtualRoutersInput{
+				MeshName: mesh.MeshName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			for _, vr := range vrlist.VirtualRouters {
+
+				rlist, err := appmeshClient.ListRoutes(&awsappmesh.ListRoutesInput{
+					VirtualRouterName: vr.VirtualRouterName,
+					MeshName:          mesh.MeshName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+				for _, r := range rlist.Routes {
+					_, err = appmeshClient.DeleteRoute(&awsappmesh.DeleteRouteInput{
+						MeshName:          mesh.MeshName,
+						VirtualRouterName: vr.VirtualRouterName,
+						RouteName:         r.RouteName,
+					})
+					Expect(err).NotTo(HaveOccurred())
+				}
+
+				_, err = appmeshClient.DeleteVirtualRouter(&awsappmesh.DeleteVirtualRouterInput{
+					MeshName:          mesh.MeshName,
+					VirtualRouterName: vr.VirtualRouterName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+			}
+
+			_, err = appmeshClient.DeleteMesh(&awsappmesh.DeleteMeshInput{
 				MeshName: mesh.MeshName,
 			})
 			Expect(err).NotTo(HaveOccurred())
 		}
 
-		return
 		testsetup.TeardownKube(namespace)
 	})
 
 	It("works", func() {
-		if false {
-			go setup.Main(func(e error) {
-				defer GinkgoRecover()
-				if e == nil {
-					return
-				}
-				if strings.Contains(e.Error(), "upstream") {
-					return
-				}
-				Fail(e.Error())
-			}, namespace)
+		go setup.Main(func(e error) {
+			defer GinkgoRecover()
+			if e == nil {
+				return
+			}
+			if strings.Contains(e.Error(), "upstream") {
+				return
+			}
+			Fail(e.Error())
+		}, namespace)
 
-			// start discovery
-			cmd := exec.Command(PathToUds, "--namespace", namespace)
-			cmd.Env = os.Environ()
-			_, err := gexec.Start(cmd, os.Stdout, os.Stdout)
-			Expect(err).NotTo(HaveOccurred())
-		}
+		// start discovery
+		cmd := exec.Command(PathToUds, "--namespace", namespace)
+		cmd.Env = os.Environ()
+		_, err := gexec.Start(cmd, os.Stdout, os.Stdout)
+		Expect(err).NotTo(HaveOccurred())
 
 		meshes, routingRules, secretClient, err := v1Clients()
 		Expect(err).NotTo(HaveOccurred())
@@ -102,7 +153,7 @@ var _ = FDescribe("appmesh routing E2e", func() {
 			return testsetup.Curl(testsetup.CurlOpts{
 				Method:  "GET",
 				Path:    "/reviews/1",
-				Service: "reviews",
+				Service: "reviews." + namespace + ".svc.cluster.local",
 				Port:    9080,
 			})
 		}, time.Second*120).Should(ContainSubstring(`{"id": "1","reviews": [{  "reviewer": "Reviewer1",  "text": "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!"},{  "reviewer": "Reviewer2",  "text": "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare."}]}`))
@@ -114,7 +165,7 @@ var _ = FDescribe("appmesh routing E2e", func() {
 			return testsetup.Curl(testsetup.CurlOpts{
 				Method:  "GET",
 				Path:    "/reviews/1",
-				Service: "reviews",
+				Service: "reviews." + namespace + ".svc.cluster.local",
 				Port:    9080,
 			})
 		}, time.Second*120).Should(ContainSubstring(`{"id": "1","reviews": [{  "reviewer": "Reviewer1",  "text": "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!", "rating": {"stars": 5, "color": "black"}},{  "reviewer": "Reviewer2",  "text": "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare.", "rating": {"stars": 4, "color": "black"}}]}`))
