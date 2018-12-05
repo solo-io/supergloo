95c4e0fc442d6f8bd59ca7b6b6d62b1d6f3f9504
Mitch Kelley <mitchdraft@users.noreply.github.com>
2018-12-05 16:19:26 -0500

    Cli better get (#85)
    * add yaml format and policy count
    
    * interactive selection of resource type
    
    * got plural and short names working
    
    * prints available resources
    
    * typo
    
    * got ahead of myself
    
    * more print stuff
    
    * formatting

--- cli/pkg/cmd/get/get.go
+++ cli/pkg/cmd/get/get.go
@@ -3,86 +3,170 @@ package get
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
+	"github.com/olekukonko/tablewriter"
+	"github.com/solo-io/supergloo/pkg/constants"
+
 	"github.com/solo-io/supergloo/cli/pkg/cmd/get/info"
-	"github.com/solo-io/supergloo/cli/pkg/cmd/get/printers"
 	"github.com/solo-io/supergloo/cli/pkg/common"
 
 	"github.com/solo-io/solo-kit/pkg/errors"
 	"github.com/solo-io/supergloo/cli/pkg/cmd/options"
 	"github.com/spf13/cobra"
+	"gopkg.in/AlecAivazis/survey.v1"
+	k8s "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var supportedOutputFormats = []string{"wide"}
+var supportedOutputFormats = []string{"wide", "yaml"}
 
 func Cmd(opts *options.Options) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "get",
 		Short: `Display one or many supergloo resources`,
 		Long:  `Display one or many supergloo resources`,
-		Args:  cobra.RangeArgs(1, 2),
-		Run: func(c *cobra.Command, args []string) {
+		Args:  cobra.RangeArgs(0, 2),
+		RunE: func(c *cobra.Command, args []string) error {
 			if err := get(args, opts); err != nil {
-				fmt.Println(err)
+				return err
 			}
+			return nil
 		},
 	}
 	getOpts := &opts.Get
-	pFlags := cmd.Flags()
-	pFlags.StringVarP(&getOpts.Output, "output", "o", "",
-		"Output format. Must be one of: \n"+strings.Join(supportedOutputFormats, "|"))
+	flags := cmd.Flags()
+	flags.StringVarP(&getOpts.Output, "output", "o", "",
+		"Output format. Options include: \n"+strings.Join(supportedOutputFormats, "|"))
+
+	cmd.AddCommand(getResourcesCmd(opts))
+
+	return cmd
+}
+
+func getResourcesCmd(opts *options.Options) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "resources",
+		Short:   `Displays resources that can be displayed`,
+		Aliases: []string{"r", "options"},
+		Args:    cobra.ExactArgs(0),
+		RunE: func(c *cobra.Command, args []string) error {
+			crdClient, err := common.GetKubeCrdClient()
+			if err != nil {
+				return err
+			}
+			crdList, err := (*crdClient).List(k8s.ListOptions{})
+			if err != nil {
+				return fmt.Errorf("Error retrieving supergloo resource types. Cause: %v \n", err)
+			}
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetBorder(false)
+			table.SetHeader([]string{"", "resource", "plural", "short names"})
+			index := 1
+			for _, crd := range crdList.Items {
+				if strings.Contains(crd.Name, common.SuperglooGroupName) {
+					// TODO (EItanya) think of a better way to deal with this
+					nameSpec := crd.Spec.Names
+					if nameSpec.Singular != "install" {
+						table.Append([]string{strconv.Itoa(index), nameSpec.Singular, nameSpec.Plural, strings.Join(nameSpec.ShortNames, ",")})
+						index++
+					}
+				}
+			}
+			table.Render()
+			return nil
+		},
+	}
+	getOpts := &opts.Get
+	flags := cmd.Flags()
+	flags.StringVarP(&getOpts.Output, "output", "o", "",
+		"Output format. Options include: \n"+strings.Join(supportedOutputFormats, "|"))
+
+	flags.StringVarP(&getOpts.Namespace, "namespace", "n", constants.SuperglooNamespace, "namespace to search")
 	return cmd
 }
 
 func get(args []string, opts *options.Options) error {
 
-	output := opts.Get.Output
-	if output != "" && !common.Contains(supportedOutputFormats, output) {
-		return errors.Errorf(common.UnknownOutputFormat, output, strings.Join(supportedOutputFormats, "|"))
-	}
-
-	if argNumber := len(args); argNumber == 1 {
-		return getResource(args[0], "", opts.Get)
-	} else {
-		// Show the resource of the given type with the given name
-		return getResource(args[0], args[1], opts.Get)
-	}
-}
-
-func getResource(resourceType, resourceName string, opts options.Get) error {
 	infoClient, err := info.NewClient()
 	if err != nil {
 		return err
 	}
 
+	if err := ensureParameters(infoClient, opts, args); err != nil {
+		return err
+	}
+
+	return getResource(infoClient, opts.Get)
+}
+
+func ensureParameters(infoClient info.SuperglooInfoClient, opts *options.Options, args []string) error {
+	gOpts := &opts.Get
+
 	// Get available resource types
-	resourceTypes, err := infoClient.ListResourceTypes()
-	if err != nil {
-		return err
-	}
+	resourceTypes := infoClient.ListResourceTypes()
 
-	// Validate input resource type
-	if !common.Contains(resourceTypes, resourceType) {
-		return errors.Errorf(common.UnknownResourceTypeMsg, resourceType)
-	}
-
-	// Fetch the resource information
-	resourceInfo, err := infoClient.ListResources(resourceType, resourceName)
-	if err != nil {
-		return err
-	}
-
-	// Write the resource information to stdout
-	writer := printers.NewTableWriter(os.Stdout)
-	if err = writer.WriteLine(resourceInfo.Headers(opts)); err != nil {
-		return err
-	}
-	for _, line := range resourceInfo.Resources(opts) {
-		if err = writer.WriteLine(line); err != nil {
+	// Argument count is validated by cobra.RangeArgs
+	if len(args) == 0 {
+		if err := selectResourceInteractive(resourceTypes, opts); err != nil {
 			return err
+		}
+	} else {
+
+		// first arg is the resource type
+		gOpts.Type = args[0]
+		// second arg is the resource name (optional)
+		gOpts.Name = ""
+		if len(args) == 2 {
+			gOpts.Name = args[1]
 		}
 	}
 
-	return writer.Flush()
+	// Validate input resource type
+	if !common.Contains(resourceTypes, gOpts.Type) {
+		return errors.Errorf(common.UnknownResourceTypeMsg, gOpts.Type)
+	}
+
+	// Output format is set by a flag
+	if gOpts.Output != "" && !common.Contains(supportedOutputFormats, gOpts.Output) {
+		return errors.Errorf(common.UnknownOutputFormat, gOpts.Output, strings.Join(supportedOutputFormats, "|"))
+	}
+
+	return nil
+}
+
+func getResource(infoClient info.SuperglooInfoClient, gOpts options.Get) error {
+
+	// Fetch the resource information
+	err := infoClient.ListResources(gOpts)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func selectResourceInteractive(resourceTypes []string, opts *options.Options) error {
+	chosenResourceType, err := chooseResourceType(resourceTypes)
+	if err != nil {
+		return err
+	}
+	opts.Get.Type = chosenResourceType
+	return nil
+}
+
+func chooseResourceType(resourceTypes []string) (string, error) {
+
+	question := &survey.Select{
+		Message: "Select a resource type",
+		Options: resourceTypes,
+	}
+
+	var choice string
+	if err := survey.AskOne(question, &choice, survey.Required); err != nil {
+		// this should not error
+		fmt.Println("error with input")
+		return "", err
+	}
+
+	return choice, nil
 }
