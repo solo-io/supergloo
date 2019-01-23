8155cbd9e4245ef2afe88173e581543ae4228555
Scott Weiss <sdw35@cornell.edu>
2019-01-23 15:01:57 -0500

    delete vendor dir
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- vendor/github.com/ilackarms/protokit/update-imports.sh
+++
@@ -1,4 +0,0 @@
-
-for i in $(find . | grep .go); do sed -i 's@golang/protobuf@gogo/protobuf@g' "$i"; done
-for i in $(find . | grep .go); do sed -i 's@protoc-gen-go@protoc-gen-gogo@g' "$i"; done
-for i in $(find . | grep .go); do sed -i 's@pseudomuto@ilackarms@g' "$i"; done
