124bfd5b149a017fce1b147255e9405993649681
Scott Weiss <sdw35@cornell.edu>
2019-02-25 12:19:16 -0500

    install syncer, test is wip
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ pkg/install/istio/syncer_test.go
@@ -0,0 +1,12 @@
+package istio_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/solo-io/supergloo/pkg/install/istio"
+)
+
+var _ = Describe("Syncer", func() {
+
+})
