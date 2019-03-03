0845d618c1c9d75208b49a508576e80dbc36694f
Scott Weiss <sdw35@cornell.edu>
2019-03-03 15:07:33 -0500

    update makefile, add ci steps, add ci scripts (#193)
    * update makefile, add ci steps, add ci scripts
    
    Signed-off-by: soloio-bot <sdw35@cornell.edu>
    
    * this is how you make the ci caches
    
    Signed-off-by: soloio-bot <sdw35@cornell.edu>
    
    * move to new docs push fmt, fix comments
    
    Signed-off-by: soloio-bot <sdw35@cornell.edu>

---
+++ ci/push_docs.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/solo-io/go-utils/docsutils"
+)
+
+func main() {
+	spec := docsutils.DocsPRSpec{
+		Owner:           "solo-io",
+		Repo:            "supergloo",
+		Product:         "supergloo",
+		ChangelogPrefix: "supergloo",
+		ApiPaths: []string{
+			"docs/v1/github.com/solo-io/supergloo",
+			"docs/v1/github.com/solo-io/solo-kit",
+			"docs/v1/gogoproto",
+			"docs/v1/google",
+		},
+	}
+	docsutils.PushDocsCli(&spec)
+}
