35176c032742721f7821261cc9ce62a51af11473
scott weiss <sdw35@cornell.edu>
2019-03-08 18:01:58 -0500

    create syncer to wrap translator
    Signed-off-by: scott weiss <sdw35@cornell.edu>

---
+++ pkg/translator/istio/syncer_reconcilers.go
@@ -0,0 +1,151 @@
+package istio
+
+import (
+	"context"
+
+	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/go-utils/errors"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	policyv1alpha1 "github.com/solo-io/supergloo/pkg/api/external/istio/authorization/v1alpha1"
+	"github.com/solo-io/supergloo/pkg/api/external/istio/networking/v1alpha3"
+	rbacv1alpha1 "github.com/solo-io/supergloo/pkg/api/external/istio/rbac/v1alpha1"
+)
+
+type Reconcilers interface {
+	ReconcileAll(ctx context.Context, writeNamespace string, config *MeshConfig) error
+}
+
+type istioReconcilers struct {
+	ownerLabels map[string]string
+
+	rbacConfigReconciler         rbacv1alpha1.RbacConfigReconciler
+	serviceRoleReconciler        rbacv1alpha1.ServiceRoleReconciler
+	serviceRoleBindingReconciler rbacv1alpha1.ServiceRoleBindingReconciler
+	meshPolicyReconciler         policyv1alpha1.MeshPolicyReconciler
+	destinationRuleReconciler    v1alpha3.DestinationRuleReconciler
+	virtualServiceReconciler     v1alpha3.VirtualServiceReconciler
+}
+
+func NewIstioReconcilers(ownerLabels map[string]string,
+	rbacConfigReconciler rbacv1alpha1.RbacConfigReconciler,
+	serviceRoleReconciler rbacv1alpha1.ServiceRoleReconciler,
+	serviceRoleBindingReconciler rbacv1alpha1.ServiceRoleBindingReconciler,
+	meshPolicyReconciler policyv1alpha1.MeshPolicyReconciler,
+	destinationRuleReconciler v1alpha3.DestinationRuleReconciler,
+	virtualServiceReconciler v1alpha3.VirtualServiceReconciler) Reconcilers {
+	return &istioReconcilers{
+		ownerLabels:                  ownerLabels,
+		rbacConfigReconciler:         rbacConfigReconciler,
+		serviceRoleReconciler:        serviceRoleReconciler,
+		serviceRoleBindingReconciler: serviceRoleBindingReconciler,
+		meshPolicyReconciler:         meshPolicyReconciler,
+		destinationRuleReconciler:    destinationRuleReconciler,
+		virtualServiceReconciler:     virtualServiceReconciler,
+	}
+}
+
+func (s *istioReconcilers) ReconcileAll(ctx context.Context, writeNamespace string, config *MeshConfig) error {
+	logger := contextutils.LoggerFrom(ctx)
+
+	logger.Infof("MeshPolicy: %v", config.MeshPolicy.Metadata.Name)
+	s.setLabels(config.MeshPolicy)
+	if err := s.meshPolicyReconciler.Reconcile(
+		writeNamespace,
+		policyv1alpha1.MeshPolicyList{config.MeshPolicy}, // mesh policy is a singleton
+		nil,
+		clients.ListOpts{
+			Ctx:      ctx,
+			Selector: s.ownerLabels,
+		},
+	); err != nil {
+		return errors.Wrapf(err, "reconciling default mesh policy")
+	}
+
+	logger.Infof("RbacConfig: %v", config.RbacConfig.Metadata.Name)
+	s.setLabels(config.RbacConfig)
+	if err := s.rbacConfigReconciler.Reconcile(
+		writeNamespace,
+		rbacv1alpha1.RbacConfigList{config.RbacConfig}, // rbac config is a singleton
+		nil,
+		clients.ListOpts{
+			Ctx:      ctx,
+			Selector: s.ownerLabels,
+		},
+	); err != nil {
+		return errors.Wrapf(err, "reconciling default rbac config")
+	}
+
+	logger.Infof("DestinationRules: %v", config.DestinationRules.Names())
+	s.setLabels(config.DestinationRules.AsResources()...)
+	if err := s.destinationRuleReconciler.Reconcile(
+		writeNamespace,
+		config.DestinationRules,
+		nil,
+		clients.ListOpts{
+			Ctx:      ctx,
+			Selector: s.ownerLabels,
+		},
+	); err != nil {
+		return errors.Wrapf(err, "reconciling destination rules")
+	}
+
+	logger.Infof("VirtualServices: %v", config.VirtualServices.Names())
+	s.setLabels(config.VirtualServices.AsResources()...)
+	if err := s.virtualServiceReconciler.Reconcile(
+		writeNamespace,
+		config.VirtualServices,
+		nil,
+		clients.ListOpts{
+			Ctx:      ctx,
+			Selector: s.ownerLabels,
+		},
+	); err != nil {
+		return errors.Wrapf(err, "reconciling virtual services")
+	}
+
+	logger.Infof("ServiceRoles: %v", config.ServiceRoles.Names())
+	s.setLabels(config.ServiceRoles.AsResources()...)
+	if err := s.serviceRoleReconciler.Reconcile(
+		writeNamespace,
+		config.ServiceRoles,
+		nil,
+		clients.ListOpts{
+			Ctx:      ctx,
+			Selector: s.ownerLabels,
+		},
+	); err != nil {
+		return errors.Wrapf(err, "reconciling service roles")
+	}
+
+	logger.Infof("ServiceRoleBindings: %v", config.ServiceRoleBindings.Names())
+	s.setLabels(config.ServiceRoleBindings.AsResources()...)
+	if err := s.serviceRoleBindingReconciler.Reconcile(
+		writeNamespace,
+		config.ServiceRoleBindings,
+		nil,
+		clients.ListOpts{
+			Ctx:      ctx,
+			Selector: s.ownerLabels,
+		},
+	); err != nil {
+		return errors.Wrapf(err, "reconciling service role bindings")
+	}
+
+	return nil
+}
+
+// set labels on all resources, required for our reconciler
+func (s *istioReconcilers) setLabels(rcs ...resources.Resource) {
+	for _, res := range rcs {
+		resources.UpdateMetadata(res, func(meta *core.Metadata) {
+			if meta.Labels == nil {
+				meta.Labels = make(map[string]string)
+			}
+			for k, v := range s.ownerLabels {
+				meta.Labels[k] = v
+			}
+		})
+	}
+}
---
+++ pkg/translator/istio/syncer_reconcilers_test.go
@@ -0,0 +1,110 @@
+package istio_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/factory"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"github.com/solo-io/solo-kit/test/helpers"
+	. "github.com/solo-io/supergloo/pkg/translator/istio"
+
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/memory"
+	policyv1alpha1 "github.com/solo-io/supergloo/pkg/api/external/istio/authorization/v1alpha1"
+	"github.com/solo-io/supergloo/pkg/api/external/istio/networking/v1alpha3"
+	rbacv1alpha1 "github.com/solo-io/supergloo/pkg/api/external/istio/rbac/v1alpha1"
+)
+
+var _ = Describe("SyncerReconcilers", func() {
+	var (
+		rbacConfigClient         rbacv1alpha1.RbacConfigClient
+		serviceRoleClient        rbacv1alpha1.ServiceRoleClient
+		serviceRoleBindingClient rbacv1alpha1.ServiceRoleBindingClient
+		meshPolicyClient         policyv1alpha1.MeshPolicyClient
+		destinationRuleClient    v1alpha3.DestinationRuleClient
+		virtualServiceClient     v1alpha3.VirtualServiceClient
+
+		rec Reconcilers
+	)
+	BeforeEach(func() {
+		mem := &factory.MemoryResourceClientFactory{Cache: memory.NewInMemoryResourceCache()}
+
+		var err error
+		rbacConfigClient, err = rbacv1alpha1.NewRbacConfigClient(mem)
+		Expect(err).NotTo(HaveOccurred())
+		rbacConfigReconciler := rbacv1alpha1.NewRbacConfigReconciler(rbacConfigClient)
+
+		serviceRoleClient, err = rbacv1alpha1.NewServiceRoleClient(mem)
+		Expect(err).NotTo(HaveOccurred())
+		serviceRoleReconciler := rbacv1alpha1.NewServiceRoleReconciler(serviceRoleClient)
+
+		serviceRoleBindingClient, err = rbacv1alpha1.NewServiceRoleBindingClient(mem)
+		Expect(err).NotTo(HaveOccurred())
+		serviceRoleBindingReconciler := rbacv1alpha1.NewServiceRoleBindingReconciler(serviceRoleBindingClient)
+
+		meshPolicyClient, err = policyv1alpha1.NewMeshPolicyClient(mem)
+		Expect(err).NotTo(HaveOccurred())
+		meshPolicyReconciler := policyv1alpha1.NewMeshPolicyReconciler(meshPolicyClient)
+
+		destinationRuleClient, err = v1alpha3.NewDestinationRuleClient(mem)
+		Expect(err).NotTo(HaveOccurred())
+		destinationRuleReconciler := v1alpha3.NewDestinationRuleReconciler(destinationRuleClient)
+
+		virtualServiceClient, err = v1alpha3.NewVirtualServiceClient(mem)
+		Expect(err).NotTo(HaveOccurred())
+		virtualServiceReconciler := v1alpha3.NewVirtualServiceReconciler(virtualServiceClient)
+
+		labels := map[string]string{"test": "labels"}
+		rec = NewIstioReconcilers(labels,
+			rbacConfigReconciler,
+			serviceRoleReconciler,
+			serviceRoleBindingReconciler,
+			meshPolicyReconciler,
+			destinationRuleReconciler,
+			virtualServiceReconciler,
+		)
+	})
+
+	It("reconciles from a set of MeshConfig", func() {
+		writeNs := "write-namespace"
+		mpMeta, drMeta, vsMeta, rbMeta, srMeta, srbMeta := randomMeta(writeNs), randomMeta(writeNs), randomMeta(writeNs), randomMeta(writeNs), randomMeta(writeNs), randomMeta(writeNs)
+		config := &MeshConfig{
+			MeshPolicy:       &policyv1alpha1.MeshPolicy{Metadata: mpMeta},
+			DestinationRules: []*v1alpha3.DestinationRule{{Metadata: drMeta}},
+			VirtualServices:  []*v1alpha3.VirtualService{{Metadata: vsMeta}},
+			SecurityConfig: SecurityConfig{
+				ServiceRoles:        []*rbacv1alpha1.ServiceRole{{Metadata: srMeta}},
+				ServiceRoleBindings: []*rbacv1alpha1.ServiceRoleBinding{{Metadata: srbMeta}},
+				RbacConfig:          &rbacv1alpha1.RbacConfig{Metadata: rbMeta},
+			},
+		}
+
+		err := rec.ReconcileAll(context.TODO(), writeNs, config)
+		Expect(err).NotTo(HaveOccurred())
+
+		// reconciler should have created each type of resource
+		_, err = meshPolicyClient.Read(mpMeta.Name, clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = rbacConfigClient.Read(rbMeta.Name, clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = destinationRuleClient.Read(drMeta.Namespace, drMeta.Name, clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = virtualServiceClient.Read(vsMeta.Namespace, vsMeta.Name, clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = serviceRoleClient.Read(srMeta.Namespace, srMeta.Name, clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = serviceRoleBindingClient.Read(srbMeta.Namespace, srbMeta.Name, clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
+
+func randomMeta(writeNamespace string) core.Metadata {
+	return core.Metadata{Name: "a" + helpers.RandString(4), Namespace: writeNamespace}
+}
