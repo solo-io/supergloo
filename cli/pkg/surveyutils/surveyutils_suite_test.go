eccd6757093aa59093d499e7f5c52e0d28d7c0fa
scott weiss <sdw35@cornell.edu>
2019-03-13 15:21:23 -0400

    skip interactive tests in ci
    Signed-off-by: scott weiss <sdw35@cornell.edu>

--- cli/pkg/surveyutils/surveyutils_suite_test.go
+++ cli/pkg/surveyutils/surveyutils_suite_test.go
@@ -1,6 +1,8 @@
 package surveyutils_test
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -8,6 +10,11 @@ import (
 )
 
 func TestSurveyutils(t *testing.T) {
+	// we allow disabling interactive tests as they appear to fail our current ci environment
+	// tracking with https://github.com/solo-io/supergloo/issues/243
+	if os.Getenv("SKIP_INTERACTIVE_TESTS") == "1" {
+		log.Printf("Skipping interactive tests.")
+	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Surveyutils Suite")
 }
