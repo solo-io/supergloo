f3be78ef166f68b803b0db8a19e9b80e77b9070f
scott weiss <sdw35@cornell.edu>
2019-03-13 17:13:46 -0400

    add lock to install test
    Signed-off-by: scott weiss <sdw35@cornell.edu>

--- cli/pkg/cmd/initialize/initialize_suite_test.go
+++ cli/pkg/cmd/initialize/initialize_suite_test.go
@@ -3,6 +3,8 @@ package initialize_test
 import (
 	"testing"
 
+	"github.com/solo-io/supergloo/test/testutils"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +13,19 @@ func TestInitialize(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Initialize Suite")
 }
+
+var (
+	lock *testutils.TestClusterLocker
+	err  error
+)
+
+var _ = BeforeSuite(func() {
+	kubeClient := testutils.MustKubeClient()
+	lock, err = testutils.NewTestClusterLocker(kubeClient, "default")
+	Expect(err).NotTo(HaveOccurred())
+	Expect(lock.AcquireLock()).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	Expect(lock.ReleaseLock()).NotTo(HaveOccurred())
+})
