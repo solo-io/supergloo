f743ccea80a6be2ab23b15746ab5bd21afe0c483
EItanya <eitan.yarmush@solo.io>
2019-03-13 21:22:00 -0400

    clean up after refactor

--- pkg2/constants/constants.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-var (
-	SuperglooNamespace = "supergloo-system"
-	MeshOptions        = []string{"istio", "consul", "linkerd2", "appmesh"}
-	ConsulInstallPath  = "https://s3.amazonaws.com/supergloo.solo.io/consul.tar.gz"
-	IstioInstallPath   = "https://s3.amazonaws.com/supergloo.solo.io/istio-1.0.3.tgz"
-	LinkerdInstallPath = "https://s3.amazonaws.com/supergloo.solo.io/linkerd2-0.1.1.tgz"
-)
