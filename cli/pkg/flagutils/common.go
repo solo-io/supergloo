3dea6f3da8f8cd1853905b2eca829e094e0fcfb1
Scott Weiss <sdw35@cornell.edu>
2019-03-15 12:46:39 -0400

    add kubeyaml flag for printing kube yaml (#258)
    * add kubeyaml flag for printing kube yaml
    
    Signed-off-by: scott weiss <sdw35@cornell.edu>
    
    * add test suite so tests can run
    
    Signed-off-by: scott weiss <sdw35@cornell.edu>
    
    * changelog
    
    Signed-off-by: scott weiss <sdw35@cornell.edu>

--- cli/pkg/flagutils/common.go
+++ cli/pkg/flagutils/common.go
@@ -2,6 +2,7 @@ package flagutils
 
 import (
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"github.com/solo-io/supergloo/cli/pkg/options"
 	"github.com/spf13/pflag"
 )
 
@@ -16,4 +17,9 @@ func AddInteractiveFlag(set *pflag.FlagSet, boolptr *bool) {
 func AddMetadataFlags(set *pflag.FlagSet, in *core.Metadata) {
 	set.StringVar(&in.Name, "name", "", "name for the resource")
 	set.StringVar(&in.Namespace, "namespace", "supergloo-system", "namespace for the resource")
+}
+
+func AddKubeYamlFlag(set *pflag.FlagSet, opts *options.Options) {
+	set.BoolVar(&opts.PrintKubeYaml, "kubeyaml", false, "if true, this command will print "+
+		"the yaml used to create a kubernetes resource rather than directly trying to create/apply the resource")
 }
