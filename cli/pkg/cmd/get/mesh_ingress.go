7a85b0f97b8ccb8544dee64c773a94bffd55f408
EItanya <eitan.yarmush@solo.io>
2019-03-31 11:22:28 -0400

    gloo proxy url

---
+++ cli/pkg/cmd/get/mesh_ingress.go
@@ -0,0 +1,11 @@
+package get
+
+import (
+	"github.com/solo-io/supergloo/cli/pkg/options"
+	"github.com/spf13/cobra"
+)
+
+func getMeshIngressCmd(opts *options.Options) *cobra.Command {
+	cmd := &cobra.Command{}
+	return cmd
+}
---
+++ cli/pkg/cmd/get/root.go
@@ -0,0 +1,27 @@
+package get
+
+import (
+	"github.com/solo-io/supergloo/cli/pkg/options"
+	"github.com/spf13/cobra"
+)
+
+func Cmd(opts *options.Options) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "get",
+		Aliases: []string{"g"},
+		Short:   "get information about supergloo objects",
+		// 		Long: `Creates or updates Rule resources which the SuperGloo controller
+		// will use to configure an installed mesh.
+		//
+		// This set of commands creates Kubernetes CRDs which the SuperGloo controller
+		// reads asynchronously.
+		//
+		// To view these crds:
+		//
+		// kubectl get routingrule [-n supergloo-system]
+		//
+		// `,
+	}
+
+	return cmd
+}
