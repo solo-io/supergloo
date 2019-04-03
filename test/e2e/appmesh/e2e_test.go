fe90c04bd840083b93a70316b5d3a2c8bb65ea21
EItanya <eitan.yarmush@solo.io>
2019-04-03 17:37:58 -0400

    more work on appmesh e2e

--- test/e2e/appmesh/e2e_test.go
+++ test/e2e/appmesh/e2e_test.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
+	skclients "github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	"github.com/solo-io/supergloo/cli/pkg/helpers/clients"
 	"github.com/solo-io/supergloo/install/helm/supergloo/generate"
+	sgutils "github.com/solo-io/supergloo/test/e2e/utils"
 	sgtestutils "github.com/solo-io/supergloo/test/testutils"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -50,8 +54,38 @@ func testRegisterAppmesh(meshName, secretName string) {
 	region, vnLabel := "us-east-1", "vn"
 	err := utils.Supergloo(fmt.Sprintf("register appmesh --name %s --region %s "+
 		"--secret %s --select-namespaces %s --virtual-node-label %s", meshName, region, secretName, namespaceWithInject, vnLabel))
-
 	Expect(err).NotTo(HaveOccurred())
+
+	meshClient := clients.MustMeshClient()
+	Eventually(func() error {
+		_, err := meshClient.Read(superglooNamespace, meshName, skclients.ReadOpts{})
+		return err
+	}).ShouldNot(HaveOccurred())
+
+	err = sgutils.DeployTestRunner(basicNamespace)
+	Expect(err).NotTo(HaveOccurred())
+
+	// the sidecar injector might take some time to become available
+	Eventually(func() error {
+		return sgutils.DeployTestRunner(namespaceWithInject)
+	}, time.Minute*1).ShouldNot(HaveOccurred())
+
+	err = sgutils.DeployBookInfo(namespaceWithInject)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = sgtestutils.WaitUntilPodsRunning(time.Minute*4, basicNamespace,
+		"testrunner",
+	)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = sgtestutils.WaitUntilPodsRunning(time.Minute*2, namespaceWithInject,
+		"testrunner",
+		"reviews-v1",
+		"reviews-v2",
+		"reviews-v3",
+	)
+	Expect(err).NotTo(HaveOccurred())
+
 }
 
 func testUnregisterAppmesh(meshName string) {
