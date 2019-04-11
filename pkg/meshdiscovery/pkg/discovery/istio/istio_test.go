d5ec2577b919dbcd74d2e2e5925eca237814ca92
EItanya <eitan.yarmush@solo.io>
2019-04-11 17:17:16 -0400

    some more unit tests for version pulling logic

--- pkg/meshdiscovery/pkg/discovery/istio/istio_test.go
+++ pkg/meshdiscovery/pkg/discovery/istio/istio_test.go
@@ -68,5 +68,23 @@ var _ = Describe("istio mesh discovery unit tests", func() {
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("unable to find image version for image"))
 		})
+		It("fails when image is the incorrect format", func() {
+			container := kubev1.Container{
+				Image: "istio-pilot:10.6",
+			}
+			pod := constructPod(container)
+			_, err := getVersionFromPod(pod)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("unable to find image version for image"))
+		})
+		It("errors when no version is found in image name", func() {
+			container := kubev1.Container{
+				Image: "istio-pilot:1.0.6",
+			}
+			pod := constructPod(container)
+			version, err := getVersionFromPod(pod)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(version).To(Equal("1.0.6"))
+		})
 	})
 })
