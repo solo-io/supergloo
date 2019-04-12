fab33fdb8f74b70417f02b1e965802017156ccdb
EItanya <eitan.yarmush@solo.io>
2019-04-12 11:23:09 -0400

    removed unused mockgen code

--- pkg/meshdiscovery/pkg/discovery/interface.go
+++ pkg/meshdiscovery/pkg/discovery/interface.go
@@ -6,8 +6,6 @@ import (
 	v1 "github.com/solo-io/supergloo/pkg/api/v1"
 )
 
-//go:generate mockgen -destination mocks_test.go  -source interface.go -package discovery
-
 type MeshDiscovery interface {
 	DiscoverMeshes(ctx context.Context, snapshot *v1.DiscoverySnapshot) (v1.MeshList, error)
 }
