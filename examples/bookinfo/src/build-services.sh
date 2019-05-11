de6349832ff29838a5c9d9eab5729592b69fff88
Mitch Kelley <mitch.kelley@solo.io>
2019-05-11 14:36:21 -0400

    port build script

--- examples/bookinfo/src/build-services.sh
+++ examples/bookinfo/src/build-services.sh
@@ -24,21 +24,21 @@ fi
 
 VERSION=$1
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-REPO_SPEC="docker.io/mitchdraft"
+REPO_SPEC="soloio"
 
 
 pushd "$SCRIPTDIR/reviews"
   #java build the app.
   docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
   pushd reviews-wlpcfg
-#    #plain build -- no ratings
-#    docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v1:${VERSION}" -t istio/examples-bookinfo-reviews-v1:latest --build-arg service_version=v1 .
-#    #with ratings black stars
-#    docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v2:${VERSION}" -t istio/examples-bookinfo-reviews-v2:latest --build-arg service_version=v2 \
-#	   --build-arg enable_ratings=true .
-#    #with ratings red stars
-#    docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v3:${VERSION}" -t istio/examples-bookinfo-reviews-v3:latest --build-arg service_version=v3 \
-#	   --build-arg enable_ratings=true --build-arg star_color=red .
+    #plain build -- no ratings
+  docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v1:${VERSION}" -t "${REPO_SPEC}/examples-bookinfo-reviews-v1:latest" --build-arg service_version=v1 .
+    #with ratings black stars
+    docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v2:${VERSION}" -t "${REPO_SPEC}/examples-bookinfo-reviews-v2:latest" --build-arg service_version=v2 \
+	   --build-arg enable_ratings=true .
+    #with ratings red stars
+    docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v3:${VERSION}" -t "${REPO_SPEC}/examples-bookinfo-reviews-v3:latest" --build-arg service_version=v3 \
+	   --build-arg enable_ratings=true --build-arg star_color=red .
     #with ratings red stars, with cascading failure
     docker build -t "${REPO_SPEC}/examples-bookinfo-reviews-v4:${VERSION}" -t "${REPO_SPEC}/examples-bookinfo-reviews-v4:latest" --build-arg service_version=v4 \
 	   --build-arg enable_ratings=true --build-arg star_color=red --build-arg propagate_failure=true .
