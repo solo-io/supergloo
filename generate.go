8de6f6b933a35f7bfc61fa02d99a3a750b0d8d2e
Scott Weiss <sdw35@cornell.edu>
2019-05-20 23:56:15 +0200

    fix test, fix compile

--- generate.go
+++ generate.go
@@ -23,7 +23,7 @@ func main() {
 		CompileProtos:      true,
 		GenDocs:            docsOpts,
 		CustomImports:      []string{"../gloo"},
-		SkipGenMocks:       true,
+		SkipGenMocks:       false,
 		SkipGeneratedTests: true,
 	}); err != nil {
 		log.Fatalf("generate failed!: %v", err)
