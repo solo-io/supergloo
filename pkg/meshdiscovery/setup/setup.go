333902955332d11d45bd25f11d7b0640885bb0ab
Scott Weiss <sdw35@cornell.edu>
2019-06-12 14:56:36 -0400

    fix linkerd e2e test, wire up discovery

--- pkg/meshdiscovery/setup/setup.go
+++ pkg/meshdiscovery/setup/setup.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"os"
 
+	"github.com/solo-io/supergloo/pkg/meshdiscovery/linkerd"
+
 	"github.com/solo-io/supergloo/pkg/meshdiscovery/istio"
 
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients/wrapper"
@@ -56,12 +58,19 @@ func createRootContext(customCtx context.Context) context.Context {
 
 func runDiscoveryEventLoop(ctx context.Context, writeNamespace string, cs *clientset.Clientset, istioClients *clientset.IstioClientset, errHandler func(error)) error {
 
+	meshReconciler := v1.NewMeshReconciler(cs.Discovery.Mesh)
+
 	istioDiscovery := istio.NewIstioDiscoverySyncer(
 		writeNamespace,
-		v1.NewMeshReconciler(cs.Discovery.Mesh),
+		meshReconciler,
 		istioClients.MeshPolicies,
 		cs.ApiExtensions.ApiextensionsV1beta1().CustomResourceDefinitions(),
 		cs.Kube.BatchV1(),
+	)
+
+	linkerdDiscovery := linkerd.NewLinkerdDiscoverySyncer(
+		writeNamespace,
+		meshReconciler,
 	)
 
 	emitter := v1.NewDiscoverySimpleEmitter(wrapper.AggregatedWatchFromClients(
@@ -71,7 +80,10 @@ func runDiscoveryEventLoop(ctx context.Context, writeNamespace string, cs *clien
 		wrapper.ClientWatchOpts{BaseClient: cs.Input.ConfigMap.BaseClient()},
 		wrapper.ClientWatchOpts{BaseClient: cs.Input.TlsSecret.BaseClient()},
 	))
-	eventLoop := v1.NewDiscoverySimpleEventLoop(emitter, istioDiscovery)
+	eventLoop := v1.NewDiscoverySimpleEventLoop(emitter,
+		istioDiscovery,
+		linkerdDiscovery,
+	)
 
 	errs, err := eventLoop.Run(ctx)
 	if err != nil {
