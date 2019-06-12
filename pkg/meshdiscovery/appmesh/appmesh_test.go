a7cb24c2992bfdf4370c02b152c0c96f8b2e078c
Scott Weiss <sdw35@cornell.edu>
2019-06-12 15:40:49 -0400

    wip: appmesh discovery

---
+++ pkg/meshdiscovery/appmesh/appmesh_test.go
@@ -0,0 +1,12 @@
+package appmesh_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/solo-io/supergloo/pkg/meshdiscovery/appmesh"
+)
+
+var _ = Describe("Appmesh", func() {
+
+})
