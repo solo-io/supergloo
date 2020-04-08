4d6d001d4ded80d80fde35ec2bdbf21de3730720
EItanya <eitan.yarmush@solo.io>
2020-04-08 01:18:41 -0400

    kind fix

--- cli/pkg/tree/demo/script/RegisterCluster.bash
+++ cli/pkg/tree/demo/script/RegisterCluster.bash
@@ -4,9 +4,14 @@ meshctlBinaryName=%s
 clusterName=%s
 contextName=%s
 useDevCsrChart=%s
+kindNetwork=%s
 
 if [ -n "$useDevCsrChart" ]; then
   csrChartArg='--dev-csr-agent-chart --local-cluster-domain-override=host.docker.internal'
 fi
 
-$meshctlBinaryName cluster register --remote-context $contextName --remote-cluster-name $clusterName $csrChartArg
+if [ -n "$kindNetwork" ]; then
+  kindNetworkArg='--local-cluster-domain-override=host.docker.internal'
+fi
+
+$meshctlBinaryName cluster register --remote-context $contextName --remote-cluster-name $clusterName $csrChartArg $kindNetworkArg
