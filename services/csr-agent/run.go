f21ed9afa634fc80b2c98202e3d8570098222322
Harvey Xia <harvey@harveyxia.com>
2020-04-14 15:08:46 -0400

    fix controller-runtime v0.5 timing

--- services/csr-agent/run.go
+++ services/csr-agent/run.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	"github.com/solo-io/go-utils/contextutils"
+	security_v1alpha1 "github.com/solo-io/service-mesh-hub/pkg/api/security.zephyr.solo.io/v1alpha1"
 	"github.com/solo-io/service-mesh-hub/pkg/bootstrap"
 	"github.com/solo-io/service-mesh-hub/services/csr-agent/pkg/wire"
 	"go.uber.org/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 func Run(ctx context.Context) {
@@ -19,16 +21,7 @@ func Run(ctx context.Context) {
 		logger.Fatalw("error initializing discovery clients", zap.Error(err))
 	}
 
-	istioCsrHandler := csrAgentContext.VirtualMeshCSRDataSourceFactory(
-		contextutils.WithLogger(ctx, "csr_agent_data_source"),
-		csrAgentContext.CsrClient,
-		csrAgentContext.CsrAgentIstioProcessor,
-	)
-	if err = csrAgentContext.CsrEventWatcher.AddEventHandler(ctx, istioCsrHandler); err != nil {
-		logger.Fatalw("error VirtualMeshCSR handler", zap.Error(err))
-	}
-
-	if err = csrAgentContext.Manager.Start(); err != nil {
+	if err = csrAgentContext.Manager.Start(startComponents(csrAgentContext)); err != nil {
 		logger.Fatalw("the local manager instance failed to start up or died with an error", zap.Error(err))
 	}
 
@@ -37,5 +30,28 @@ func Run(ctx context.Context) {
 		return
 	case <-csrAgentContext.Manager.GotError():
 		logger.Fatalw("the local manager encountered an error", zap.Error(csrAgentContext.Manager.Error()))
+	}
+}
+
+// Controller-runtime Watches require the manager to be started first, otherwise it will block indefinitely
+// Thus we initialize all components (and their associated watches) as an AsyncManagerStartOptionsFunc.
+func startComponents(csrAgentContext wire.CsrAgentContext) func(context.Context, manager.Manager) error {
+	return func(ctx context.Context, m manager.Manager) error {
+		logger := contextutils.LoggerFrom(ctx)
+		// Ensure that VirtualMeshCertificateSigningRequest is registered with the controller-runtime scheme
+		err := security_v1alpha1.AddToScheme(m.GetScheme())
+		if err != nil {
+			logger.Errorf("error adding security to scheme", zap.Error(err))
+			return err
+		}
+		istioCsrHandler := csrAgentContext.VirtualMeshCSRDataSourceFactory(
+			contextutils.WithLogger(ctx, "csr_agent_data_source"),
+			csrAgentContext.CsrClient,
+			csrAgentContext.CsrAgentIstioProcessor,
+		)
+		if err := csrAgentContext.CsrEventWatcher.AddEventHandler(ctx, istioCsrHandler); err != nil {
+			logger.Fatalw("error VirtualMeshCSR handler", zap.Error(err))
+		}
+		return nil
 	}
 }
