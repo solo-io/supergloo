0049a022ffaccf29c9ce5d205a9abbd57e7047c6
Harvey Xia <harvey@harveyxia.com>
2020-04-23 13:55:41 -0400

    add tests, wip

---
+++ services/mesh-discovery/pkg/rest-api/aws/appmesh-client/appmesh_client_factory.go
@@ -0,0 +1,32 @@
+package appmesh_client
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/appmesh"
+	"github.com/solo-io/service-mesh-hub/cli/pkg/common/aws_creds"
+	v1 "k8s.io/api/core/v1"
+)
+
+type appMeshClientFactory struct {
+	secretConverter aws_creds.SecretAwsCredsConverter
+}
+
+func NewAppMeshClientFactory(secretConverter aws_creds.SecretAwsCredsConverter) AppMeshClientFactory {
+	return &appMeshClientFactory{secretConverter: secretConverter}
+}
+
+func (a *appMeshClientFactory) Build(secret *v1.Secret, region string) (*appmesh.AppMesh, error) {
+	creds, err := a.secretConverter.SecretToCreds(secret)
+	if err != nil {
+		return nil, err
+	}
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: creds,
+		Region:      aws.String(region),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return appmesh.New(sess), nil
+}
