131eea2813747dfeffd08be020e95dd305c714ae
Harvey Xia <harvey@harveyxia.com>
2020-04-23 11:18:59 -0400

    change proto name

--- cli/pkg/common/table_printing/mesh_printer.go
+++ cli/pkg/common/table_printing/mesh_printer.go
@@ -207,7 +207,7 @@ func (m *meshPrinter) printAwsAppMeshMeshes(out io.Writer, meshes []*zephyr_disc
 		newRow = append(
 			newRow,
 			awsAppMesh.GetName(),
-			awsAppMesh.GetApiName(),
+			awsAppMesh.GetAwsAccountName(),
 			awsAppMesh.GetRegion(),
 		)
 
