fa9c917b93f9817dc1b99b1f20b9bf5bcb53b914
Harvey Xia <harvey@harveyxia.com>
2020-04-24 08:49:28 -0400

    generate global mocks in parallel (#625)
    * generate global mocks in parallel
    * goimports

--- codegen/global_mocks/global_mocks.go
+++ codegen/global_mocks/global_mocks.go
@@ -8,28 +8,4 @@ package global_mocks
 *
 ************************************************************************************************/
 
-//go:generate mockgen -package mock_controller_runtime -destination ../../test/mocks/controller-runtime/mock_manager.go sigs.k8s.io/controller-runtime/pkg/manager Manager
-//go:generate mockgen -package mock_controller_runtime -destination ../../test/mocks/controller-runtime/mock_predicate.go sigs.k8s.io/controller-runtime/pkg/predicate Predicate
-//go:generate mockgen -package mock_controller_runtime -destination ../../test/mocks/controller-runtime/mock_cache.go sigs.k8s.io/controller-runtime/pkg/cache Cache
-//go:generate mockgen -package mock_controller_runtime -destination ../../test/mocks/controller-runtime/mock_dynamic_client.go  sigs.k8s.io/controller-runtime/pkg/client Client,StatusWriter
-//go:generate mockgen -package mock_cli_runtime -destination ../../test/mocks/cli_runtime/mock_rest_client_getter.go k8s.io/cli-runtime/pkg/resource RESTClientGetter
-//go:generate mockgen -package mock_corev1 -destination ../../test/mocks/corev1/mock_service_controller.go github.com/solo-io/service-mesh-hub/pkg/api/kubernetes/core/v1/controller ServiceEventWatcher
-//go:generate mockgen -package mock_zephyr_discovery -destination ../../test/mocks/zephyr/discovery/mock_mesh_workload_controller.go github.com/solo-io/service-mesh-hub/pkg/api/discovery.zephyr.solo.io/v1alpha1/controller MeshWorkloadEventWatcher,MeshServiceEventWatcher,MeshEventWatcher
-//go:generate mockgen -package mock_zephyr_networking -destination ../../test/mocks/zephyr/networking/mock_virtual_mesh_controller.go github.com/solo-io/service-mesh-hub/pkg/api/networking.zephyr.solo.io/v1alpha1/controller VirtualMeshEventWatcher,TrafficPolicyEventWatcher,AccessControlPolicyEventWatcher
-
-// Generate mock clients
-// K8s clients
-//go:generate mockgen -package mock_k8s_core_clients -destination ../../test/mocks/clients/kubernetes/core/v1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/kubernetes/core/v1 ServiceClient,PodClient,NamespaceClient,NodeClient,ServiceAccountClient,SecretClient,ConfigMapClient
-//go:generate mockgen -package mock_k8s_apps_clients -destination ../../test/mocks/clients/kubernetes/apps/v1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/kubernetes/apps/v1 DeploymentClient,ReplicaSetClient
-//go:generate mockgen -package mock_k8s_extension_clients -destination ../../test/mocks/clients/kubernetes/apiextensions.k8s.io/v1beta1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/kubernetes/apiextensions.k8s.io/v1beta1 CustomResourceDefinitionClient
-// Zephyr clients
-//go:generate mockgen -package mock_zephyr_discovery_clients -destination ../../test/mocks/clients/discovery.zephyr.solo.io/v1alpha1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/discovery.zephyr.solo.io/v1alpha1 KubernetesClusterClient,MeshClient,MeshServiceClient,MeshWorkloadClient
-//go:generate mockgen -package mock_zephyr_networking_clients -destination ../../test/mocks/clients/networking.zephyr.solo.io/v1alpha1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/networking.zephyr.solo.io/v1alpha1 TrafficPolicyClient,AccessControlPolicyClient,VirtualMeshClient
-//go:generate mockgen -package mock_zephyr_security_clients -destination ../../test/mocks/clients/security.zephyr.solo.io/v1alpha1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/security.zephyr.solo.io/v1alpha1 VirtualMeshCertificateSigningRequestClient
-// Istio clients
-//go:generate mockgen -package mock_istio_security_clients -destination ../../test/mocks/clients/istio/security/v1alpha3/clients.go github.com/solo-io/service-mesh-hub/pkg/api/istio/security/v1beta1 AuthorizationPolicyClient
-//go:generate mockgen -package mock_istio_networking_clients -destination ../../test/mocks/clients/istio/networking/v1beta1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/istio/networking/v1alpha3 DestinationRuleClient,EnvoyFilterClient,GatewayClient,ServiceEntryClient,VirtualServiceClient
-// Linkerd clients
-//go:generate mockgen -package mock_linkerd_clients -destination ../../test/mocks/clients/linkerd/v1alpha2/clients.go github.com/solo-io/service-mesh-hub/pkg/api/linkerd/v1alpha2 ServiceProfileClient
-// SMI clients
-//go:generate mockgen -package mock_smi_clients -destination ../../test/mocks/clients/smi/split/v1alpha1/clients.go github.com/solo-io/service-mesh-hub/pkg/api/smi/split/v1alpha1 TrafficSplitClient
+//go:generate ./parallel_mockgen.sh
