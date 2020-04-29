e5fe086b0c128df562e6dd5384ffc739d3e2d3a3
Harvey Xia <harvey@harveyxia.com>
2020-04-29 08:35:34 -0400

    fix test

--- services/mesh-networking/pkg/federation/decider/strategies/permissive_federation_test.go
+++ services/mesh-networking/pkg/federation/decider/strategies/permissive_federation_test.go
@@ -73,7 +73,7 @@ var _ = Describe("Permissive Federation", func() {
 			MulticlusterDnsName: "application-svc.application-ns.application-cluster",
 		}
 		meshServiceClient.EXPECT().
-			UpdateMeshService(ctx, &serviceCopy).
+			UpsertMeshServiceSpec(ctx, &serviceCopy).
 			Return(nil)
 
 		err := strategies.NewPermissiveFederation(meshServiceClient).WriteFederationToServices(ctx, vm, perMeshResources)
