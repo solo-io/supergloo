0b6f512530e39aa5a1b837fdfc8f5943376f3887
Harvey Xia <harvey@harveyxia.com>
2020-05-04 10:43:53 -0400

    gen

--- services/mesh-discovery/pkg/discovery/cluster-tenancy/k8s/mocks/mock_interfaces.go
+++ services/mesh-discovery/pkg/discovery/cluster-tenancy/k8s/mocks/mock_interfaces.go
@@ -6,12 +6,13 @@ package mock_k8s_tenancy
 
 import (
 	context "context"
+	reflect "reflect"
+
 	gomock "github.com/golang/mock/gomock"
 	v1alpha1 "github.com/solo-io/service-mesh-hub/pkg/api/discovery.zephyr.solo.io/v1alpha1"
 	controller "github.com/solo-io/service-mesh-hub/pkg/api/discovery.zephyr.solo.io/v1alpha1/controller"
 	controller0 "github.com/solo-io/service-mesh-hub/pkg/api/kubernetes/core/v1/controller"
 	v1 "k8s.io/api/core/v1"
-	reflect "reflect"
 )
 
 // MockClusterTenancyRegistrarLoop is a mock of ClusterTenancyRegistrarLoop interface.
