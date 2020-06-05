741462b3d8603d72686ae51f037297034fc77d4c
Harvey Xia <harvey@harveyxia.com>
2020-06-05 11:17:19 -0400

    add delimiter

--- pkg/metadata/appmesh.go
+++ pkg/metadata/appmesh.go
@@ -3,7 +3,7 @@ package metadata
 import zephyr_discovery "github.com/solo-io/service-mesh-hub/pkg/api/discovery.zephyr.solo.io/v1alpha1"
 
 func BuildVirtualServiceName(meshService *zephyr_discovery.MeshService) string {
-	return meshService.Spec.GetKubeService().GetRef().GetName() + meshService.Spec.GetKubeService().GetRef().GetNamespace()
+	return meshService.Spec.GetKubeService().GetRef().GetName() + "-" + meshService.Spec.GetKubeService().GetRef().GetNamespace()
 }
 
 func BuildVirtualRouterName(meshService *zephyr_discovery.MeshService) string {
