2f9e8cac206f4a9e5d3129ec9f7193d41f10f14d
Scott Weiss <sdw35@cornell.edu>
2020-06-10 15:49:31 -0400

    fix imports

--- test/e2e/appmesh_eks_test.go
+++ test/e2e/appmesh_eks_test.go
@@ -12,6 +12,8 @@ import (
 	"github.com/golang/sync/errgroup"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	smh_core "github.com/solo-io/service-mesh-hub/pkg/api/core.smh.solo.io/v1alpha1"
+	smh_networking "github.com/solo-io/service-mesh-hub/pkg/api/networking.smh.solo.io/v1alpha1"
 	"github.com/solo-io/service-mesh-hub/pkg/kube/metadata"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
