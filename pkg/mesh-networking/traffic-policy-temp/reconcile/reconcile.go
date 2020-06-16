e7404da253c5e18dfd7b7e8a6987f64605e05a5a
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-06-16 14:44:47 -0400

    Move temp traffic policy (#778)
    * move
    * rename reconciler -> processor
    * chng

---
+++ pkg/mesh-networking/traffic-policy-temp/reconcile/reconcile.go
@@ -0,0 +1,114 @@
+package reconcile
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	smh_discovery "github.com/solo-io/service-mesh-hub/pkg/api/discovery.smh.solo.io/v1alpha1"
+	smh_networking "github.com/solo-io/service-mesh-hub/pkg/api/networking.smh.solo.io/v1alpha1"
+	aggregation_framework "github.com/solo-io/service-mesh-hub/pkg/mesh-networking/traffic-policy-temp/aggregation/framework"
+	translation_framework "github.com/solo-io/service-mesh-hub/pkg/mesh-networking/traffic-policy-temp/translation/framework"
+	"github.com/solo-io/service-mesh-hub/pkg/mesh-networking/traffic-policy-temp/translation/framework/snapshot"
+	traffic_policy_validation "github.com/solo-io/service-mesh-hub/pkg/mesh-networking/traffic-policy-temp/validation"
+)
+
+type Reconciler struct {
+	trafficPolicyClient smh_networking.TrafficPolicyClient
+	meshServiceClient   smh_discovery.MeshServiceClient
+
+	snapshotReconciler snapshot.TranslationSnapshotReconciler
+
+	validationProcessor  traffic_policy_validation.ValidationProcessor
+	aggregationProcessor aggregation_framework.AggregationProcessor
+	translationProcessor translation_framework.TranslationProcessor
+}
+
+func NewReconciler(
+	trafficPolicyClient smh_networking.TrafficPolicyClient,
+	meshServiceClient smh_discovery.MeshServiceClient,
+	snapshotReconciler snapshot.TranslationSnapshotReconciler,
+	validationProcessor traffic_policy_validation.ValidationProcessor,
+	aggregationProcessor aggregation_framework.AggregationProcessor,
+	translationProcessor translation_framework.TranslationProcessor,
+) *Reconciler {
+	return &Reconciler{
+		trafficPolicyClient:  trafficPolicyClient,
+		meshServiceClient:    meshServiceClient,
+		snapshotReconciler:   snapshotReconciler,
+		validationProcessor:  validationProcessor,
+		aggregationProcessor: aggregationProcessor,
+		translationProcessor: translationProcessor,
+	}
+}
+
+func (*Reconciler) GetName() string {
+	return "traffic-policy-reconciler"
+}
+
+func (v *Reconciler) Reconcile(ctx context.Context) error {
+	var multierr error
+
+	trafficPolicies, err := v.trafficPolicyClient.ListTrafficPolicy(ctx)
+	if err != nil {
+		return err
+	}
+
+	var allTrafficPolicies []*smh_networking.TrafficPolicy
+	for _, tp := range trafficPolicies.Items {
+		trafficPolicy := tp
+		allTrafficPolicies = append(allTrafficPolicies, &trafficPolicy)
+	}
+
+	meshServiceList, err := v.meshServiceClient.ListMeshService(ctx)
+	if err != nil {
+		return err
+	}
+
+	var meshServices []*smh_discovery.MeshService
+	for _, ms := range meshServiceList.Items {
+		meshService := ms
+		meshServices = append(meshServices, &meshService)
+	}
+
+	// TODO: this works because traffic policies are not copied with the various processors;
+	// if we want to relax that constrant, we can consider generating and using the Set object
+	// for traffic policies.
+	trafficPoliciesToUpdateSet := map[*smh_networking.TrafficPolicy]bool{}
+
+	trafficPoliciesToUpdate := v.validationProcessor.Process(ctx, allTrafficPolicies, meshServices)
+	for _, tp := range trafficPoliciesToUpdate {
+		trafficPoliciesToUpdateSet[tp] = true
+	}
+
+	if objectsToUpdate, err := v.aggregationProcessor.Process(ctx, allTrafficPolicies); err == nil {
+		if objectsToUpdate != nil {
+			for _, service := range objectsToUpdate.MeshServices {
+				err := v.meshServiceClient.UpdateMeshServiceStatus(ctx, service)
+				if err != nil {
+					multierr = multierror.Append(multierr, err)
+				}
+			}
+
+			// accumulate traffic policies, so we have only one status update.
+			for _, tp := range objectsToUpdate.TrafficPolicies {
+				trafficPoliciesToUpdateSet[tp] = true
+			}
+		}
+	} else {
+		multierr = multierror.Append(multierr, err)
+	}
+
+	for trafficPolicy := range trafficPoliciesToUpdateSet {
+		err := v.trafficPolicyClient.UpdateTrafficPolicyStatus(ctx, trafficPolicy)
+		if err != nil {
+			multierr = multierror.Append(multierr, err)
+		}
+	}
+
+	if clusterNameToSnapshot, err := v.translationProcessor.Process(ctx, meshServices); err == nil {
+		v.snapshotReconciler.ReconcileAllSnapshots(ctx, clusterNameToSnapshot)
+	} else {
+		multierr = multierror.Append(multierr, err)
+	}
+	return multierr
+}
---
+++ pkg/mesh-networking/traffic-policy-temp/translation/translators/interfaces.go
@@ -0,0 +1,60 @@
+package mesh_translation
+
+import (
+	smh_discovery "github.com/solo-io/service-mesh-hub/pkg/api/discovery.smh.solo.io/v1alpha1"
+	smh_discovery_types "github.com/solo-io/service-mesh-hub/pkg/api/discovery.smh.solo.io/v1alpha1/types"
+	smh_networking_types "github.com/solo-io/service-mesh-hub/pkg/api/networking.smh.solo.io/v1alpha1/types"
+	"github.com/solo-io/service-mesh-hub/pkg/mesh-networking/traffic-policy-temp/translation/framework/snapshot"
+	istio_networking "istio.io/client-go/pkg/apis/networking/v1alpha3"
+)
+
+//go:generate mockgen -source ./interfaces.go -destination ./mocks/mock_interfaces.go
+
+/*
+ * Every translator should embed the following three interfaces, for consistency across all the different mesh-specific translators.
+ * Note that the validator is used in the aggregation validation step.
+ */
+type TranslationValidator interface {
+	// Ignore the mesh-specific output from the translation step, in an effort to unify them behind some kind of common interface.
+	// Generics ;(
+	GetTranslationErrors(
+		meshService *smh_discovery.MeshService,
+		allMeshServices []*smh_discovery.MeshService,
+		mesh *smh_discovery.Mesh,
+		trafficPolicies []*smh_discovery_types.MeshServiceStatus_ValidatedTrafficPolicy,
+	) []*TranslationError
+}
+
+type DiscoveryLabelsGetter interface {
+	// get the labels that this translator attaches to the resources it creates
+	GetTranslationLabels() map[string]string
+}
+
+type NamedTranslator interface {
+	Name() string
+}
+
+// Mesh-specific interfaces
+
+type IstioTranslator interface {
+	TranslationValidator
+	DiscoveryLabelsGetter
+	NamedTranslator
+	snapshot.TranslationSnapshotAccumulator
+	Translate(
+		meshService *smh_discovery.MeshService,
+		allMeshServices []*smh_discovery.MeshService,
+		mesh *smh_discovery.Mesh,
+		trafficPolicies []*smh_discovery_types.MeshServiceStatus_ValidatedTrafficPolicy,
+	) (*IstioTranslationOutput, []*TranslationError)
+}
+
+type TranslationError struct {
+	Policy           *smh_discovery_types.MeshServiceStatus_ValidatedTrafficPolicy
+	TranslatorErrors []*smh_networking_types.TrafficPolicyStatus_TranslatorError
+}
+
+type IstioTranslationOutput struct {
+	VirtualServices  []*istio_networking.VirtualService
+	DestinationRules []*istio_networking.DestinationRule
+}
