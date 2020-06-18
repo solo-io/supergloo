5441efbabd17f86360b678fdb011182e249fa9e5
Scott Weiss <sdw35@cornell.edu>
2020-06-18 17:48:28 -0400

    improve istio_mesh_detector

--- pkg/mesh-discovery/snapshot/translation/mesh/detector/deployment/istio/istio_mesh_detector.go
+++ pkg/mesh-discovery/snapshot/translation/mesh/detector/deployment/istio/istio_mesh_detector.go
@@ -1,12 +1,16 @@
 package istio
 
 import (
+	"fmt"
 	"github.com/rotisserie/eris"
 	corev1sets "github.com/solo-io/external-apis/pkg/api/k8s/core/v1/sets"
+	"github.com/solo-io/go-utils/kubeutils"
 	"github.com/solo-io/service-mesh-hub/pkg/api/discovery.smh.solo.io/v1alpha1"
 	"github.com/solo-io/service-mesh-hub/pkg/common/docker"
 	v1 "github.com/solo-io/skv2/pkg/api/core.skv2.solo.io/v1"
+	"github.com/solo-io/smh/pkg/common/defaults"
 	"github.com/solo-io/smh/pkg/mesh-discovery/snapshot/translation/mesh/detector/deployment"
+	"github.com/solo-io/smh/pkg/mesh-discovery/utils/labelutils"
 	istiov1alpha1 "istio.io/api/mesh/v1alpha1"
 	"istio.io/istio/pkg/util/gogoprotomarshal"
 	appsv1 "k8s.io/api/apps/v1"
@@ -16,6 +20,7 @@ import (
 )
 
 const (
+	legacyPilotDeploymentName = "istio-pilot"
 	istiodDeploymentName      = "istiod"
 	istioContainerKeyword     = "istio"
 	pilotContainerKeyword     = "pilot"
@@ -29,12 +34,16 @@ var UnexpectedPilotImageName = func(err error, imageName string) error {
 
 // detects Istio if a deployment contains the istiod container.
 type meshDetector struct {
-	configMaps      corev1sets.ConfigMapSet
-	imageNameParser docker.ImageNameParser
+	configMaps corev1sets.ConfigMapSet
 }
 
 func NewMeshDetector(configMaps corev1sets.ConfigMapSet) deployment.MeshDetector {
 	return &meshDetector{configMaps: configMaps}
+}
+
+// how to name an Istio Mesh
+func IstioMeshName(deployment *appsv1.Deployment) string {
+	return kubeutils.SanitizeNameV2(fmt.Sprintf("%v-%v-%v", deployment.Name, deployment.Namespace, deployment.ClusterName))
 }
 
 // returns nil, nil of the deployment does not contain the istiod image
@@ -54,13 +63,18 @@ func (d *meshDetector) DetectMesh(deployment *appsv1.Deployment) (*v1alpha1.Mesh
 	}
 
 	mesh := &v1alpha1.Mesh{
-		ObjectMeta: metav1.ObjectMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: defaults.GetPodNamespace(),
+			Name:      IstioMeshName(deployment),
+			Labels:    labelutils.ClusterLabels(deployment.ClusterName),
+		},
 		Spec: v1alpha1.MeshSpec{
 			MeshType: &v1alpha1.MeshSpec_Istio_{
 				Istio: &v1alpha1.MeshSpec_Istio{
 					Installation: &v1alpha1.MeshSpec_MeshInstallation{
 						Namespace: deployment.Namespace,
 						Cluster:   deployment.ClusterName,
+						Version:   version,
 					},
 					CitadelInfo: &v1alpha1.MeshSpec_Istio_CitadelInfo{
 						TrustDomain: trustDomain,
@@ -77,9 +91,8 @@ func (d *meshDetector) DetectMesh(deployment *appsv1.Deployment) (*v1alpha1.Mesh
 
 func (d *meshDetector) getIstiodVersion(deployment *appsv1.Deployment) (string, error) {
 	for _, container := range deployment.Spec.Template.Spec.Containers {
-		// Detect the vers istiod deployment (for Istio versions >= 1.5) or istio-citadel deployment (for Istio versions < 1.5)
 		if isIstiod(deployment, &container) {
-			parsedImage, err := d.imageNameParser.Parse(container.Image)
+			parsedImage, err := docker.ParseImageName(container.Image)
 			if err != nil {
 				return "", UnexpectedPilotImageName(err, container.Image)
 			}
@@ -95,7 +108,7 @@ func (d *meshDetector) getIstiodVersion(deployment *appsv1.Deployment) (string, 
 
 // Return true if deployment is inferred to be an Istiod deployment
 func isIstiod(deployment *appsv1.Deployment, container *corev1.Container) bool {
-	return deployment.GetName() == istiodDeploymentName &&
+	return (deployment.GetName() == istiodDeploymentName || deployment.GetName() == legacyPilotDeploymentName) &&
 		strings.Contains(container.Image, istioContainerKeyword) &&
 		strings.Contains(container.Image, pilotContainerKeyword)
 }
