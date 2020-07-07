15fb946af1724d0007f5b2f995c065ed7af2b326
Scott Weiss <sdw35@cornell.edu>
2020-07-07 13:55:57 -0400

    do not copy labels

--- pkg/mesh-discovery/translator/utils/object_meta.go
+++ pkg/mesh-discovery/translator/utils/object_meta.go
@@ -12,22 +12,15 @@ import (
 
 // construct an ObjectMeta for a discovered resource from a source object (the object from which the resource was discovered)
 func DiscoveredObjectMeta(sourceResource metav1.Object) metav1.ObjectMeta {
-	labels := sourceResource.GetLabels()
-	if labels == nil {
-		labels = map[string]string{}
-	}
-	for k, v := range labelutils.ClusterLabels(sourceResource.GetClusterName()) {
-		labels[k] = v
-	}
 	return metav1.ObjectMeta{
 		Namespace:   defaults.GetPodNamespace(),
 		Name:        DiscoveredResourceName(sourceResource),
-		Labels:      labels,
+		Labels:      labelutils.ClusterLabels(sourceResource.GetClusterName()),
 		Annotations: sourceResource.GetAnnotations(),
 	}
 }
 
 // util for conventionally naming discovered resources
-func DiscoveredResourceName(sourceResource ezkube.ResourceId) string {
+func DiscoveredResourceName(sourceResource ezkube.ClusterResourceId) string {
 	return kubeutils.SanitizeNameV2(fmt.Sprintf("%v-%v-%v", sourceResource.GetName(), sourceResource.GetNamespace(), sourceResource.GetClusterName()))
 }
