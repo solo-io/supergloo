54dd281bf88ecd9e3e097c023770550e8807aaf9
Jenny Shu <jenny.shu@solo.io>
2020-08-13 08:27:40 -0400

    execute setup-kind.sh

--- pkg/meshctl/commands/demo/istio-multicluster/cleanup/demo_istio_multicluster_cleanup.go
+++ pkg/meshctl/commands/demo/istio-multicluster/cleanup/demo_istio_multicluster_cleanup.go
@@ -2,6 +2,8 @@ package cleanup
 
 import (
 	"context"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -11,9 +13,17 @@ func Command(ctx context.Context) *cobra.Command {
 		Use:   "cleanup",
 		Short: "Clean up bootstrapped local resources",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			return cleanup(ctx)
 		},
 	}
 
 	return cmd
+}
+
+func cleanup(ctx context.Context) error {
+	cmd := exec.Command("bash", "./ci/setup-kind.sh", "cleanup")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	return err
 }
--- pkg/meshctl/commands/demo/istio-multicluster/install/demo_istio_multicluster_install.go
+++ pkg/meshctl/commands/demo/istio-multicluster/install/demo_istio_multicluster_install.go
@@ -2,6 +2,8 @@ package install
 
 import (
 	"context"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -11,9 +13,17 @@ func Command(ctx context.Context) *cobra.Command {
 		Use:   "install",
 		Short: "Bootstrap a multicluster Istio demo with Service Mesh Hub",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			return install(ctx)
 		},
 	}
 
 	return cmd
+}
+
+func install(ctx context.Context) error {
+	cmd := exec.Command("bash", "./ci/setup-kind.sh")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	return err
 }
