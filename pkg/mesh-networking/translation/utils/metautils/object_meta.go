9f8296d308a99c6ea98f652cdcf4ec4fc73750ea
Harvey Xia <harvey@harveyxia.com>
2020-10-27 14:39:56 -0400

    e2e

--- pkg/mesh-networking/translation/utils/metautils/object_meta.go
+++ pkg/mesh-networking/translation/utils/metautils/object_meta.go
@@ -26,18 +26,25 @@ func TranslatedObjectMeta(sourceObj ezkube.ClusterResourceId, annotations map[st
 }
 
 // construct an ObjectMeta for a resource for a federated source object
+// meshInstallation represents the mesh instance to which the object will be output
 func FederatedObjectMeta(
 	sourceObj ezkube.ClusterResourceId,
 	meshInstallation *discoveryv1alpha2.MeshSpec_MeshInstallation,
 	annotations map[string]string,
 ) metav1.ObjectMeta {
 	return metav1.ObjectMeta{
-		Name:        sourceObj.GetName(),
+		Name:        federatedObjectName(sourceObj),
 		Namespace:   meshInstallation.Namespace,
 		ClusterName: meshInstallation.Cluster,
 		Labels:      TranslatedObjectLabels(),
 		Annotations: annotations,
 	}
+}
+
+func federatedObjectName(
+	sourceObj ezkube.ClusterResourceId,
+) string {
+	return sourceObj.GetName() + "-" + sourceObj.GetClusterName()
 }
 
 // ownership label defaults to current namespace to allow multiple SMH tenancy within a cluster.
--- test/e2e/istio/federation_test.go
+++ test/e2e/istio/federation_test.go
@@ -1,9 +1,19 @@
 package istio_test
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/solo-io/service-mesh-hub/pkg/api/networking.smh.solo.io/v1alpha2"
+	"github.com/solo-io/service-mesh-hub/pkg/mesh-networking/translation/utils/metautils"
 	"github.com/solo-io/service-mesh-hub/test/data"
+	"github.com/solo-io/service-mesh-hub/test/e2e"
 	"github.com/solo-io/service-mesh-hub/test/utils"
 	v1 "github.com/solo-io/skv2/pkg/api/core.skv2.solo.io/v1"
+	istionetworkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -13,14 +23,18 @@ var _ = Describe("Federation", func() {
 	var (
 		err      error
 		manifest utils.Manifest
+		ctx      = context.Background()
 	)
 
 	AfterEach(func() {
 		manifest.Cleanup(BookinfoNamespace)
 	})
 
+	/*
+		These tests assume that federation has been established between mgmt and remote clusters.
+	*/
+
 	It("enables communication across clusters using global dns names", func() {
-		Expect(err).NotTo(HaveOccurred())
 		manifest, err = utils.NewManifest("federation-trafficpolicies.yaml")
 		Expect(err).NotTo(HaveOccurred())
 
@@ -42,6 +56,159 @@ var _ = Describe("Federation", func() {
 
 			// check we can eventually hit the v3 subset
 			Eventually(curlReviews, "30s", "1s").Should(ContainSubstring(`"color": "red"`))
+		})
+	})
+
+	It("should output a VirtualService for the federated ServiceEntry", func() {
+		By("fault injection should be applied when sending requests to a federated ServiceEntry", func() {
+			manifest, err = utils.NewManifest("federation-trafficpolicies.yaml")
+			Expect(err).NotTo(HaveOccurred())
+
+			// Create TrafficPolicy with fault injection applied to remote cluster
+			faultInjectionPolicy := &v1alpha2.TrafficPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "remote-fault-injection",
+					Namespace: BookinfoNamespace,
+				},
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "TrafficPolicy",
+					APIVersion: v1alpha2.SchemeGroupVersion.String(),
+				},
+				Spec: v1alpha2.TrafficPolicySpec{
+					SourceSelector: nil,
+					DestinationSelector: []*v1alpha2.TrafficTargetSelector{{
+						KubeServiceRefs: &v1alpha2.TrafficTargetSelector_KubeServiceRefs{
+							Services: []*v1.ClusterObjectRef{
+								{
+									Name:        "reviews",
+									Namespace:   BookinfoNamespace,
+									ClusterName: remoteClusterName,
+								},
+							},
+						},
+					}},
+					FaultInjection: &v1alpha2.TrafficPolicySpec_FaultInjection{
+						FaultInjectionType: &v1alpha2.TrafficPolicySpec_FaultInjection_Abort_{
+							&v1alpha2.TrafficPolicySpec_FaultInjection_Abort{
+								HttpStatus: http.StatusTeapot,
+							},
+						},
+						Percentage: 100,
+					},
+				},
+			}
+
+			err = manifest.AppendResources(faultInjectionPolicy)
+			Expect(err).NotTo(HaveOccurred())
+			err = manifest.KubeApply(BookinfoNamespace)
+			Expect(err).NotTo(HaveOccurred())
+
+			// ensure status is updated
+			utils.AssertTrafficPolicyStatuses(dynamicClient, BookinfoNamespace)
+
+			Eventually(curlRemoteReviews, "30s", "1s").Should(ContainSubstring("418"))
+
+			// Delete TrafficPolicy so it doesn't interfere with subsequent tests
+			manifest.KubeDelete(BookinfoNamespace)
+		})
+
+		By("traffic mirrors and shifts should use correct hostname for federated ServiceEntry", func() {
+			manifest, err = utils.NewManifest("federation-trafficpolicies.yaml")
+			Expect(err).NotTo(HaveOccurred())
+
+			// Create TrafficPolicy with fault injection applied to remote cluster
+			trafficPolicy := &v1alpha2.TrafficPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "remote-mirror-and-shift",
+					Namespace: BookinfoNamespace,
+				},
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "TrafficPolicy",
+					APIVersion: v1alpha2.SchemeGroupVersion.String(),
+				},
+				Spec: v1alpha2.TrafficPolicySpec{
+					SourceSelector: nil,
+					DestinationSelector: []*v1alpha2.TrafficTargetSelector{{
+						KubeServiceRefs: &v1alpha2.TrafficTargetSelector_KubeServiceRefs{
+							Services: []*v1.ClusterObjectRef{
+								{
+									Name:        "reviews",
+									Namespace:   BookinfoNamespace,
+									ClusterName: remoteClusterName,
+								},
+							},
+						},
+					}},
+					Mirror: &v1alpha2.TrafficPolicySpec_Mirror{
+						DestinationType: &v1alpha2.TrafficPolicySpec_Mirror_KubeService{
+							KubeService: &v1.ClusterObjectRef{
+								Name:        "reviews",
+								Namespace:   BookinfoNamespace,
+								ClusterName: mgmtClusterName,
+							},
+						},
+						Percentage: 50,
+						Port:       9080,
+					},
+					TrafficShift: &v1alpha2.TrafficPolicySpec_MultiDestination{
+						Destinations: []*v1alpha2.TrafficPolicySpec_MultiDestination_WeightedDestination{
+							{
+								DestinationType: &v1alpha2.TrafficPolicySpec_MultiDestination_WeightedDestination_KubeService{
+									KubeService: &v1alpha2.TrafficPolicySpec_MultiDestination_WeightedDestination_KubeDestination{
+										Name:        "reviews",
+										Namespace:   BookinfoNamespace,
+										ClusterName: mgmtClusterName,
+									},
+								},
+								Weight: 50,
+							},
+						},
+					},
+				},
+			}
+
+			err = manifest.AppendResources(trafficPolicy)
+			Expect(err).NotTo(HaveOccurred())
+			err = manifest.KubeApply(BookinfoNamespace)
+			Expect(err).NotTo(HaveOccurred())
+
+			// ensure status is updated
+			utils.AssertTrafficPolicyStatuses(dynamicClient, BookinfoNamespace)
+
+			var getFederatedVirtualService = func() (*istionetworkingv1alpha3.VirtualService, error) {
+				env := e2e.GetEnv()
+				vsClient := env.Management.VirtualServiceClient
+				meta := metautils.TranslatedObjectMeta(
+					&v1.ClusterObjectRef{
+						Name:        "reviews-" + remoteClusterName,
+						Namespace:   "istio-system",
+						ClusterName: mgmtClusterName,
+					},
+					nil,
+				)
+				return vsClient.GetVirtualService(ctx, client.ObjectKey{Name: meta.Name, Namespace: meta.Namespace})
+			}
+
+			Eventually(func() bool {
+				virtualService, err := getFederatedVirtualService()
+				if err != nil {
+					return false
+				}
+				if len(virtualService.Spec.Http) > 0 {
+					httpRoute := virtualService.Spec.Http[0]
+					if httpRoute.GetMirror().GetHost() != fmt.Sprintf("reviews.%s.svc.cluster.local", BookinfoNamespace) {
+						return false
+					}
+					if len(httpRoute.GetRoute()) < 1 {
+						return false
+					}
+					if httpRoute.GetRoute()[0].GetDestination().GetHost() != fmt.Sprintf("reviews.%s.svc.cluster.local", BookinfoNamespace) {
+						return false
+					}
+					return true
+				}
+				return false
+			}, "30s", "1s").Should(BeTrue())
 		})
 	})
 })
