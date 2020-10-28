99646bb28558515d58467b32dc3f353765511caa
Harvey Xia <harvey@harveyxia.com>
2020-10-28 10:53:55 -0400

    skip confirmation

--- ci/setup-funcs.sh
+++ ci/setup-funcs.sh
@@ -164,7 +164,7 @@ function install_istio_1_7() {
 
   echo "installing istio to ${cluster}..."
 
-  cat << EOF | istioctl manifest install --context "kind-${cluster}" -f -
+  cat << EOF | istioctl manifest install -y --context "kind-${cluster}" -f -
 apiVersion: install.istio.io/v1alpha1
 kind: IstioOperator
 metadata:
