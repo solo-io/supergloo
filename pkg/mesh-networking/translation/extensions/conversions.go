2a3b43cb90dbfbb5e71b003d57f409b4c2d23060
Scott Weiss <sdw35@cornell.edu>
2020-10-29 10:15:19 -0400

    input builder

--- pkg/mesh-networking/translation/extensions/conversions.go
+++ pkg/mesh-networking/translation/extensions/conversions.go
@@ -1,6 +1,7 @@
 package extensions
 
 import (
+	discoveryv1alpha2 "github.com/solo-io/service-mesh-hub/pkg/api/discovery.smh.solo.io/v1alpha2"
 	"github.com/solo-io/service-mesh-hub/pkg/api/networking.smh.solo.io/extensions/v1alpha1"
 	"github.com/solo-io/service-mesh-hub/pkg/api/networking.smh.solo.io/input"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -40,6 +41,47 @@ func InputSnapshotToProto(in input.Snapshot) *v1alpha1.DiscoverySnapshot {
 		TrafficTargets: trafficTargets,
 		Workloads:      workloads,
 	}
+}
+
+// InputSnapshotFromProto constructs a Networking input snapshot from proto Discovery Snapshot
+// This method is not intended to be used here, but called from implementating servers.
+func InputSnapshotFromProto(name string, in *v1alpha1.DiscoverySnapshot) input.Snapshot {
+	builder := input.NewInputSnapshotManualBuilder(name)
+
+	// insert meshes
+	var meshes discoveryv1alpha2.MeshSlice
+	for _, mesh := range in.Meshes {
+		meshes = append(meshes, &discoveryv1alpha2.Mesh{
+			ObjectMeta: ObjectMetaFromProto(mesh.Metadata),
+			Spec:       *mesh.Spec,
+			Status:     *mesh.Status,
+		})
+	}
+	builder.AddMeshes(meshes)
+
+	// insert trafficTargets
+	var trafficTargets discoveryv1alpha2.TrafficTargetSlice
+	for _, trafficTarget := range in.TrafficTargets {
+		trafficTargets = append(trafficTargets, &discoveryv1alpha2.TrafficTarget{
+			ObjectMeta: ObjectMetaFromProto(trafficTarget.Metadata),
+			Spec:       *trafficTarget.Spec,
+			Status:     *trafficTarget.Status,
+		})
+	}
+	builder.AddTrafficTargets(trafficTargets)
+
+	// insert workloads
+	var workloads discoveryv1alpha2.WorkloadSlice
+	for _, workload := range in.Workloads {
+		workloads = append(workloads, &discoveryv1alpha2.Workload{
+			ObjectMeta: ObjectMetaFromProto(workload.Metadata),
+			Spec:       *workload.Spec,
+			Status:     *workload.Status,
+		})
+	}
+	builder.AddWorkloads(workloads)
+
+	return builder.Build()
 }
 
 // ObjectMetaToProto constructs a proto-compatible version of a k8s ObjectMeta
