62f8f7bb88debec21de370850781dbe10e5ca266
Scott Weiss <sdw35@cornell.edu>
2020-11-12 14:09:20 -0600

    make ci script re-runnable

--- ci/setup-funcs.sh
+++ ci/setup-funcs.sh
@@ -4,21 +4,6 @@
 # Functions for setting up kind clusters
 #####################################
 
-#!/bin/bash
-
-INSTALL_DIR=${PROJECT_ROOT}/install
-AGENT_VALUES=${INSTALL_DIR}/helm/cert-agent/values.yaml
-AGENT_IMAGE_REGISTRY=$(cat ${AGENT_VALUES} | grep "registry: " | awk '{print $2}')
-AGENT_IMAGE_REPOSITORY=$(cat ${AGENT_VALUES} | grep "repository: " | awk '{print $2}')
-AGENT_IMAGE_TAG=$(cat ${AGENT_VALUES} | grep "tag: " | awk '{print $2}' | sed 's/"//g')
-
-AGENT_IMAGE=${AGENT_IMAGE_REGISTRY}/${AGENT_IMAGE_REPOSITORY}:${AGENT_IMAGE_TAG}
-AGENT_CHART=${INSTALL_DIR}/helm/_output/charts/cert-agent/cert-agent-${AGENT_IMAGE_TAG}.tgz
-
-GLOOMESH_VALUES=${INSTALL_DIR}/helm/gloo-mesh/values.yaml
-GLOOMESH_IMAGE_TAG=$(cat ${GLOOMESH_VALUES} | grep -m 1 "tag: " | awk '{print $2}' | sed 's/"//g')
-GLOOMESH_CHART=${INSTALL_DIR}/helm/_output/charts/gloo-mesh/gloo-mesh-${GLOOMESH_IMAGE_TAG}.tgz
-
 #### FUNCTIONS
 
 function create_kind_cluster() {
@@ -410,10 +395,11 @@ function register_cluster() {
 }
 
 function install_gloomesh() {
+
   cluster=$1
   apiServerAddress=$(get_api_address ${cluster})
 
-  bash ${PROJECT_ROOT}/ci/setup-gloomesh.sh ${cluster} ${GLOOMESH_CHART} ${AGENT_CHART} ${AGENT_IMAGE} ${apiServerAddress}
+  bash ${PROJECT_ROOT}/ci/setup-gloomesh.sh ${cluster} ${apiServerAddress}
 
   if [ ! -z ${POST_INSTALL_SCRIPT} ]; then
     bash ${POST_INSTALL_SCRIPT}
--- ci/setup-gloomesh.sh
+++ ci/setup-gloomesh.sh
@@ -7,10 +7,7 @@
 #####################################
 
 cluster=$1
-glooMeshChart=$2
-agentChart=$3
-agentImage=$4
-apiServerAddress=$5
+apiServerAddress=$2
 
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
@@ -24,10 +21,22 @@ echo "deploying gloo-mesh to ${cluster} from local images..."
 
 ## build and load GlooMesh docker images
 MAKE="make -C $PROJECT_ROOT"
-eval "${MAKE} manifest-gen package-helm build-all-images -B"
+eval "${MAKE} clean-helm manifest-gen package-helm build-all-images -B"
 
 INSTALL_DIR="${PROJECT_ROOT}/install/"
 DEFAULT_MANIFEST="${INSTALL_DIR}/gloo-mesh-default.yaml"
+
+AGENT_VALUES=${INSTALL_DIR}/helm/cert-agent/values.yaml
+AGENT_IMAGE_REGISTRY=$(cat ${AGENT_VALUES} | grep "registry: " | awk '{print $2}')
+AGENT_IMAGE_REPOSITORY=$(cat ${AGENT_VALUES} | grep "repository: " | awk '{print $2}')
+AGENT_IMAGE_TAG=$(cat ${AGENT_VALUES} | grep "tag: " | awk '{print $2}' | sed 's/"//g')
+
+agentChart=${INSTALL_DIR}/helm/_output/charts/cert-agent/cert-agent-${AGENT_IMAGE_TAG}.tgz
+agentImage=${AGENT_IMAGE_REGISTRY}/${AGENT_IMAGE_REPOSITORY}:${AGENT_IMAGE_TAG}
+
+GLOOMESH_VALUES=${INSTALL_DIR}/helm/gloo-mesh/values.yaml
+GLOOMESH_IMAGE_TAG=$(cat ${GLOOMESH_VALUES} | grep -m 1 "tag: " | awk '{print $2}' | sed 's/"//g')
+glooMeshChart=${INSTALL_DIR}/helm/_output/charts/gloo-mesh/gloo-mesh-${GLOOMESH_IMAGE_TAG}.tgz
 
 # load GlooMesh discovery and networking images
 grep "image:" "${DEFAULT_MANIFEST}" \
