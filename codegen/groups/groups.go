ffb923357424b0c9f17f37b96769509bbcbbcafe
Harvey Xia <harvey@harveyxia.com>
2020-12-02 10:52:55 -0500

    remove import from codegen/groups

--- codegen/groups/groups.go
+++ codegen/groups/groups.go
@@ -1,5 +1,11 @@
 package groups
 
+/*
+	DO NOT IMPORT THIS PACKAGE
+	This package imports the "github.com/solo-io/skv2/contrib" package, which
+	will panic when skv2 template files are not found in the executing environment.
+ */
+
 import (
 	"github.com/solo-io/gloo-mesh/codegen/constants"
 	"github.com/solo-io/skv2/codegen/model"
