811f93307f02a70aa6098ff78223a863b2354f16
Harvey Xia <harvey@harveyxia.com>
2020-12-03 14:06:01 -0500

    Remote agent crds (#1112)
    * separate chart for agent CRDs
    * update helm.mk
    * update registration logic
    * tweaks
    * cl
    * update helm gitignore
    * remove agent-crds as dependency to cert-agent
    * tweak makefile
    * Merge branch 'main' into remote-agent-crds
    * Merge branch 'main' into remote-agent-crds
    * remove unneeded gitignore
    * Merge branch 'main' into remote-agent-crds

--- pkg/meshctl/commands/install/internal/flags/flags.go
+++ pkg/meshctl/commands/install/internal/flags/flags.go
@@ -30,6 +30,7 @@ type RegistrationOptions struct {
 	ClusterName         string
 	ApiServerAddress    string
 	ClusterDomain       string
+	AgentCrdsChartPath  string
 	CertAgentChartPath  string
 	CertAgentValuesPath string
 }
@@ -48,6 +49,7 @@ func (o *Options) AddToFlags(flags *pflag.FlagSet) {
 		"Name with which to register the cluster running Gloo Mesh, only applies if --register is also set")
 	flags.StringVar(&o.ApiServerAddress, "api-server-address", "", "Swap out the address of the remote cluster's k8s API server for the value of this flag. Set this flag when the address of the cluster domain used by the Gloo Mesh is different than that specified in the local kubeconfig.")
 	flags.StringVar(&o.ClusterDomain, "cluster-domain", "", "The Cluster Domain used by the Kubernetes DNS Service in the registered cluster. Defaults to 'cluster.local'. Read more: https://kubernetes.io/docs/tasks/administer-cluster/dns-custom-nameservers/")
+	flags.StringVar(&o.AgentCrdsChartPath, "agent-crds-chart-file", "", "Path to a local Helm chart for installing CRDs needed by remote agents. If unset, this command will install the agent CRDs from the publicly released Helm chart.")
 	flags.StringVar(&o.CertAgentChartPath, "cert-agent-chart-file", "", "Path to a local Helm chart for installing the Certificate Agent. If unset, this command will install the Certificate Agent from the publicly released Helm chart.")
 	flags.StringVar(&o.CertAgentValuesPath, "cert-agent-chart-values", "", "Path to a Helm values.yaml file for customizing the installation of the Certificate Agent. If unset, this command will install the Certificate Agent with default Helm values.")
 	flags.BoolVarP(&o.Verbose, "verbose", "v", false, "Enable verbose output")
@@ -84,6 +86,7 @@ func (o *Options) GetRegistrationOptions() registration.RegistrantOptions {
 			APIServerAddress: o.ApiServerAddress,
 			ClusterDomain:    o.ClusterDomain,
 		},
+		AgentCrdsChartPath: o.AgentCrdsChartPath,
 		CertAgent: registration.AgentInstallOptions{
 			ChartPath:   o.CertAgentChartPath,
 			ChartValues: o.CertAgentValuesPath,
