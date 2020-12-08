b24497e20aa194315269551c3f05ad8b675da7bf
Harvey Xia <harvey@harveyxia.com>
2020-12-08 16:09:49 -0500

    grant discovery permissions to list settings

--- pkg/meshctl/registration/register.go
+++ pkg/meshctl/registration/register.go
@@ -16,7 +16,6 @@ import (
 var gloomeshRbacRequirements = func() []rbacv1.PolicyRule {
 	var policyRules []rbacv1.PolicyRule
 	policyRules = append(policyRules, io.DiscoveryRemoteInputTypes.RbacPoliciesWatch()...)
-	policyRules = append(policyRules, io.DiscoveryLocalInputTypes.RbacPoliciesWatch()...)
 	policyRules = append(policyRules, io.LocalNetworkingOutputTypes.Snapshot.RbacPoliciesWrite()...)
 	policyRules = append(policyRules, io.IstioNetworkingOutputTypes.Snapshot.RbacPoliciesWrite()...)
 	policyRules = append(policyRules, io.SmiNetworkingOutputTypes.Snapshot.RbacPoliciesWrite()...)
