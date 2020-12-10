49d615c3f2ed4f7b02b255a3a7315e8d9dd650a2
Joe Kelley <jsphklly@gmail.com>
2020-12-10 09:35:44 -0500

    add warning log for chart overrides when extender isn't present

--- pkg/meshctl/registration/register.go
+++ pkg/meshctl/registration/register.go
@@ -199,6 +199,9 @@ func (r *Registrant) installWasmAgent(ctx context.Context, enterpriseExtenderVer
 		} else {
 			return eris.New("Failed to install Wasm Agent: no Enterprise Extender detected and no chart override provided.")
 		}
+	} else {
+		logrus.Warn("Gloo Mesh Enterprise Extender not detected. Wasm Agent installation will proceed because a chart " +
+			"override was provided, but Wasm features depend on the presence of the Extender.")
 	}
 
 	return gloomesh.Installer{
