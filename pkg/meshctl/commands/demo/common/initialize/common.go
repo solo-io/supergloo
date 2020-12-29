ab7042f6c81be5a2f328831c08c742c00abf3985
Ryan King <ryan.king@solo.io>
2020-12-29 16:48:11 -0500

    Add enterprise option to demo commands (#1183)
    * add enterprise option to demo commands
    * codegen
    * Merge branch 'main' into 1158_enterprise_demo
    * codegen with correct protoc version

--- pkg/meshctl/commands/demo/common/initialize/common.go
+++ pkg/meshctl/commands/demo/common/initialize/common.go
@@ -1,8 +1,10 @@
 package initialize
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 
@@ -12,6 +14,7 @@ import (
 	"github.com/solo-io/gloo-mesh/pkg/common/defaults"
 	"github.com/solo-io/gloo-mesh/pkg/common/version"
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/install/gloomesh"
+	installhelm "github.com/solo-io/gloo-mesh/pkg/meshctl/install/helm"
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/registration"
 	"github.com/solo-io/skv2/pkg/multicluster/register"
 )
@@ -45,111 +48,86 @@ func createKindCluster(cluster string, port string, box packr.Box) error {
 
 func installGlooMesh(ctx context.Context, cluster string, box packr.Box) error {
 	fmt.Printf("Deploying Gloo Mesh to %s from images\n", cluster)
-
-	apiServerAddress, err := getApiAddress(cluster, box)
-	if err != nil {
-		return err
-	}
-
-	kubeConfig := ""
-	kubeContext := fmt.Sprintf("kind-%s", cluster)
-
-	namespace := defaults.DefaultPodNamespace
-	verbose := true
-	gloomeshChartUri := fmt.Sprintf(gloomesh.GlooMeshChartUriTemplate, version.Version)
-	certAgentChartUri := fmt.Sprintf(gloomesh.CertAgentChartUriTemplate, version.Version)
-
-	err = gloomesh.Installer{
-		HelmChartPath:  gloomeshChartUri,
-		HelmValuesPath: "",
-		KubeConfig:     "",
-		KubeContext:    kubeContext,
-		Namespace:      namespace,
-		ReleaseName:    helm.Chart.Data.Name,
-		Verbose:        verbose,
-		DryRun:         false,
-	}.InstallGlooMesh(
-		ctx,
-	)
-	if err != nil {
+	if err := getGlooMeshInstaller(
+		cluster, gloomesh.GlooMeshChartUriTemplate,
+		version.Version,
+		nil,
+	).InstallGlooMesh(ctx); err != nil {
 		return eris.Wrap(err, "Error installing Gloo Mesh")
 	}
 
-	registrantOpts := &registration.RegistrantOptions{
-		KubeConfigPath: kubeConfig,
-		MgmtContext:    kubeContext,
-		RemoteContext:  kubeContext,
-		Registration: register.RegistrationOptions{
-			ClusterName:      cluster,
-			RemoteCtx:        kubeContext,
-			Namespace:        namespace,
-			RemoteNamespace:  namespace,
-			APIServerAddress: apiServerAddress,
-			ClusterDomain:    "",
-		},
-		CertAgent: registration.AgentInstallOptions{
-			ChartPath:   certAgentChartUri,
-			ChartValues: "",
-		},
-		Verbose: verbose,
-	}
-
-	registrant, err := registration.NewRegistrant(registrantOpts)
-	if err != nil {
-		return eris.Wrapf(err, "initializing registrant for cluster %s", cluster)
-	}
-	err = registrant.RegisterCluster(ctx)
-	if err != nil {
-		return eris.Wrapf(err, "registering cluster %s", cluster)
-	}
-
-	script, err := box.FindString("post_install_gloomesh.sh")
-	if err != nil {
-		return eris.Wrap(err, "Error loading script")
-	}
-
-	cmd := exec.Command("bash", "-c", script, cluster)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
-		return eris.Wrap(err, "Error running post-install script")
+	if err := glooMeshPostInstall(cluster, box); err != nil {
+		return err
 	}
 
 	fmt.Printf("Successfully set up Gloo Mesh on cluster %s\n", cluster)
 	return nil
 }
 
-func registerCluster(ctx context.Context, mgmtCluster string, cluster string, box packr.Box) error {
-	fmt.Printf("Registering cluster %s with cert-agent image\n", cluster)
+func installGlooMeshEnterprise(ctx context.Context, cluster, version, licenseKey string, box packr.Box) error {
+	fmt.Printf("Deploying Gloo Mesh Enterprise to %s from images\n", cluster)
+	if version == "" {
+		v, err := installhelm.GetLatestChartVersion(gloomesh.GlooMeshEnterpriseRepoURI, "gloo-mesh-enterprise")
+		if err != nil {
+			return err
+		}
+		version = v
+	}
 
+	if err := getGlooMeshInstaller(
+		cluster, gloomesh.GlooMeshEnterpriseChartUriTemplate,
+		version,
+		map[string]string{"licenseKey": licenseKey},
+	).InstallGlooMeshEnterprise(ctx); err != nil {
+		return eris.Wrap(err, "Error installing Gloo Mesh")
+	}
+
+	fmt.Printf("Successfully set up Gloo Mesh on cluster %s\n", cluster)
+	return nil
+}
+
+func getGlooMeshInstaller(cluster, chartTemplate, chartVersion string, values map[string]string) gloomesh.Installer {
+	return gloomesh.Installer{
+		HelmChartPath:  fmt.Sprintf(chartTemplate, chartVersion),
+		HelmValuesPath: "",
+		KubeConfig:     "",
+		KubeContext:    fmt.Sprintf("kind-%s", cluster),
+		Namespace:      defaults.DefaultPodNamespace,
+		ReleaseName:    helm.Chart.Data.Name,
+		Values:         values,
+		Verbose:        true,
+		DryRun:         false,
+	}
+}
+
+func registerCluster(ctx context.Context, mgmtCluster, cluster string, installWasmAgent bool, box packr.Box) error {
+	fmt.Printf("Registering cluster %s with cert-agent image\n", cluster)
 	apiServerAddress, err := getApiAddress(cluster, box)
 	if err != nil {
 		return err
 	}
 
-	kubeConfig := ""
 	mgmtKubeContext := fmt.Sprintf("kind-%s", mgmtCluster)
 	remoteKubeContext := fmt.Sprintf("kind-%s", cluster)
 
-	namespace := defaults.DefaultPodNamespace
-	certAgentChartUri := fmt.Sprintf(gloomesh.CertAgentChartUriTemplate, version.Version)
-
 	registrantOpts := &registration.RegistrantOptions{
-		KubeConfigPath: kubeConfig,
+		KubeConfigPath: "",
 		MgmtContext:    mgmtKubeContext,
 		RemoteContext:  remoteKubeContext,
 		Registration: register.RegistrationOptions{
 			ClusterName:      cluster,
 			RemoteCtx:        remoteKubeContext,
-			Namespace:        namespace,
-			RemoteNamespace:  namespace,
+			Namespace:        defaults.DefaultPodNamespace,
+			RemoteNamespace:  defaults.DefaultPodNamespace,
 			APIServerAddress: apiServerAddress,
 			ClusterDomain:    "",
 		},
 		CertAgent: registration.AgentInstallOptions{
-			ChartPath:   certAgentChartUri,
+			ChartPath:   fmt.Sprintf(gloomesh.CertAgentChartUriTemplate, version.Version),
 			ChartValues: "",
+		},
+		WasmAgent: registration.AgentInstallOptions{
+			Install: installWasmAgent,
 		},
 		Verbose: true,
 	}
@@ -158,8 +136,7 @@ func registerCluster(ctx context.Context, mgmtCluster string, cluster string, bo
 	if err != nil {
 		return eris.Wrapf(err, "initializing registrant for cluster %s", cluster)
 	}
-	err = registrant.RegisterCluster(ctx)
-	if err != nil {
+	if err := registrant.RegisterCluster(ctx); err != nil {
 		return eris.Wrapf(err, "registering cluster %s", cluster)
 	}
 
@@ -168,30 +145,38 @@ func registerCluster(ctx context.Context, mgmtCluster string, cluster string, bo
 }
 
 func getApiAddress(cluster string, box packr.Box) (string, error) {
-	script, err := box.FindString("get_api_address.sh")
-	if err != nil {
-		return "", eris.Wrap(err, "Error loading script")
-	}
-	cmd := exec.Command("bash", "-c", script, cluster)
-	bytes, err := cmd.Output()
-	if err != nil {
+	var buf bytes.Buffer
+	if err := runScript(box, &buf, "get_api_address.sh", cluster); err != nil {
 		return "", eris.Wrap(err, "Error getting API server address")
 	}
 
-	return string(bytes), nil
+	return buf.String(), nil
 }
 
 func switchContext(cluster string, box packr.Box) error {
-	script, err := box.FindString("switch_context.sh")
-	if err != nil {
-		return err
-	}
-	cmd := exec.Command("bash", "-c", script, cluster)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
+	if err := runScript(box, os.Stdout, "switch_context.sh", cluster); err != nil {
 		return eris.Wrapf(err, "Could not switch context to %s", fmt.Sprintf("kind-%s", cluster))
 	}
+
 	return nil
+}
+
+func glooMeshPostInstall(cluster string, box packr.Box) error {
+	if err := runScript(box, os.Stdout, "post_install_gloomesh.sh", cluster); err != nil {
+		return eris.Wrap(err, "Error running post-install script")
+	}
+
+	return nil
+}
+
+func runScript(box packr.Box, out io.Writer, script string, args ...string) error {
+	script, err := box.FindString(script)
+	if err != nil {
+		return eris.Wrap(err, "Error loading script")
+	}
+
+	cmd := exec.Command("bash", append([]string{"-c", script}, args...)...)
+	cmd.Stdout = out
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
