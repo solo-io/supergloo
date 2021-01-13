73bde30c3dabaa586b7feebf289d0f1b1d6973e3
Harvey Xia <harvey@harveyxia.com>
2021-01-13 11:29:34 -0500

    add AccessLogCollection status

--- docs/content/reference/api/github.com.solo-io.gloo-mesh.api.enterprise.observability.v1alpha1.access_logging.md
+++ docs/content/reference/api/github.com.solo-io.gloo-mesh.api.enterprise.observability.v1alpha1.access_logging.md
@@ -19,6 +19,7 @@ title: "access_logging.proto"
 
 ## Table of Contents
   - [AccessLogCollectionSpec](#observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionSpec)
+  - [AccessLogCollectionStatus](#observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionStatus)
 
 
 
@@ -36,6 +37,21 @@ title: "access_logging.proto"
 | ----- | ---- | ----- | ----------- |
 | workloadSelectors | [][networking.mesh.gloo.solo.io.WorkloadSelector]({{< versioned_link_path fromRoot="/reference/api/github.com.solo-io.gloo-mesh.api.networking.v1alpha2.selectors#networking.mesh.gloo.solo.io.WorkloadSelector" >}}) | repeated | Select the workloads to be configured to emit access logs. Leave empty to apply to all workloads managed by Gloo Mesh. |
   | accessLogFilters | [][envoy.config.accesslog.v3.AccessLogFilter]({{< versioned_link_path fromRoot="/reference/api/envoy.config.accesslog.v3.accesslog#envoy.config.accesslog.v3.AccessLogFilter" >}}) | repeated | Configure the criteria for determining which access logs will be emitted. Leave empty to emit all access logs. |
+  
+
+
+
+
+
+<a name="observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionStatus"></a>
+
+### AccessLogCollectionStatus
+
+
+
+| Field | Type | Label | Description |
+| ----- | ---- | ----- | ----------- |
+| observedGeneration | int64 |  | The most recent generation observed in the the AccessLogCollection metadata. If the observedGeneration does not match generation, the controller has not processed the most recent version of this resource. |
   
 
 
--- pkg/api/observability.enterprise.mesh.gloo.solo.io/v1alpha1/access_logging.pb.go
+++ pkg/api/observability.enterprise.mesh.gloo.solo.io/v1alpha1/access_logging.pb.go
@@ -87,6 +87,56 @@ func (x *AccessLogCollectionSpec) GetAccessLogFilters() []*v3.AccessLogFilter {
 	return nil
 }
 
+type AccessLogCollectionStatus struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// The most recent generation observed in the the AccessLogCollection metadata.
+	// If the observedGeneration does not match generation, the controller has not processed the most
+	// recent version of this resource.
+	ObservedGeneration int64 `protobuf:"varint,1,opt,name=observed_generation,json=observedGeneration,proto3" json:"observed_generation,omitempty"`
+}
+
+func (x *AccessLogCollectionStatus) Reset() {
+	*x = AccessLogCollectionStatus{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *AccessLogCollectionStatus) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*AccessLogCollectionStatus) ProtoMessage() {}
+
+func (x *AccessLogCollectionStatus) ProtoReflect() protoreflect.Message {
+	mi := &file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use AccessLogCollectionStatus.ProtoReflect.Descriptor instead.
+func (*AccessLogCollectionStatus) Descriptor() ([]byte, []int) {
+	return file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *AccessLogCollectionStatus) GetObservedGeneration() int64 {
+	if x != nil {
+		return x.ObservedGeneration
+	}
+	return 0
+}
+
 var File_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto protoreflect.FileDescriptor
 
 var file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_rawDesc = []byte{
@@ -118,13 +168,18 @@ var file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_acce
 	0x65, 0x6e, 0x76, 0x6f, 0x79, 0x2e, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e, 0x61, 0x63, 0x63,
 	0x65, 0x73, 0x73, 0x6c, 0x6f, 0x67, 0x2e, 0x76, 0x33, 0x2e, 0x41, 0x63, 0x63, 0x65, 0x73, 0x73,
 	0x4c, 0x6f, 0x67, 0x46, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x52, 0x10, 0x61, 0x63, 0x63, 0x65, 0x73,
-	0x73, 0x4c, 0x6f, 0x67, 0x46, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x73, 0x42, 0x5a, 0x5a, 0x58, 0x67,
-	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x6f, 0x6c, 0x6f, 0x2d, 0x69,
-	0x6f, 0x2f, 0x67, 0x6c, 0x6f, 0x6f, 0x2d, 0x6d, 0x65, 0x73, 0x68, 0x2f, 0x70, 0x6b, 0x67, 0x2f,
-	0x61, 0x70, 0x69, 0x2f, 0x6f, 0x62, 0x73, 0x65, 0x72, 0x76, 0x61, 0x62, 0x69, 0x6c, 0x69, 0x74,
-	0x79, 0x2e, 0x65, 0x6e, 0x74, 0x65, 0x72, 0x70, 0x72, 0x69, 0x73, 0x65, 0x2e, 0x6d, 0x65, 0x73,
-	0x68, 0x2e, 0x67, 0x6c, 0x6f, 0x6f, 0x2e, 0x73, 0x6f, 0x6c, 0x6f, 0x2e, 0x69, 0x6f, 0x2f, 0x76,
-	0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x73, 0x4c, 0x6f, 0x67, 0x46, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x73, 0x22, 0x4c, 0x0a, 0x19, 0x41,
+	0x63, 0x63, 0x65, 0x73, 0x73, 0x4c, 0x6f, 0x67, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x69,
+	0x6f, 0x6e, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x2f, 0x0a, 0x13, 0x6f, 0x62, 0x73, 0x65,
+	0x72, 0x76, 0x65, 0x64, 0x5f, 0x67, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x12, 0x6f, 0x62, 0x73, 0x65, 0x72, 0x76, 0x65, 0x64, 0x47,
+	0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x42, 0x5a, 0x5a, 0x58, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x6f, 0x6c, 0x6f, 0x2d, 0x69, 0x6f, 0x2f,
+	0x67, 0x6c, 0x6f, 0x6f, 0x2d, 0x6d, 0x65, 0x73, 0x68, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x61, 0x70,
+	0x69, 0x2f, 0x6f, 0x62, 0x73, 0x65, 0x72, 0x76, 0x61, 0x62, 0x69, 0x6c, 0x69, 0x74, 0x79, 0x2e,
+	0x65, 0x6e, 0x74, 0x65, 0x72, 0x70, 0x72, 0x69, 0x73, 0x65, 0x2e, 0x6d, 0x65, 0x73, 0x68, 0x2e,
+	0x67, 0x6c, 0x6f, 0x6f, 0x2e, 0x73, 0x6f, 0x6c, 0x6f, 0x2e, 0x69, 0x6f, 0x2f, 0x76, 0x31, 0x61,
+	0x6c, 0x70, 0x68, 0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -139,15 +194,16 @@ func file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_acc
 	return file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_rawDescData
 }
 
-var file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_goTypes = []interface{}{
 	(*AccessLogCollectionSpec)(nil),   // 0: observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionSpec
-	(*v1alpha2.WorkloadSelector)(nil), // 1: networking.mesh.gloo.solo.io.WorkloadSelector
-	(*v3.AccessLogFilter)(nil),        // 2: envoy.config.accesslog.v3.AccessLogFilter
+	(*AccessLogCollectionStatus)(nil), // 1: observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionStatus
+	(*v1alpha2.WorkloadSelector)(nil), // 2: networking.mesh.gloo.solo.io.WorkloadSelector
+	(*v3.AccessLogFilter)(nil),        // 3: envoy.config.accesslog.v3.AccessLogFilter
 }
 var file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_depIdxs = []int32{
-	1, // 0: observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionSpec.workload_selectors:type_name -> networking.mesh.gloo.solo.io.WorkloadSelector
-	2, // 1: observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionSpec.access_log_filters:type_name -> envoy.config.accesslog.v3.AccessLogFilter
+	2, // 0: observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionSpec.workload_selectors:type_name -> networking.mesh.gloo.solo.io.WorkloadSelector
+	3, // 1: observability.enterprise.mesh.gloo.solo.io.AccessLogCollectionSpec.access_log_filters:type_name -> envoy.config.accesslog.v3.AccessLogFilter
 	2, // [2:2] is the sub-list for method output_type
 	2, // [2:2] is the sub-list for method input_type
 	2, // [2:2] is the sub-list for extension type_name
@@ -175,6 +231,18 @@ func file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_acc
 				return nil
 			}
 		}
+		file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*AccessLogCollectionStatus); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -182,7 +250,7 @@ func file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_acc
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_github_com_solo_io_gloo_mesh_api_enterprise_observability_v1alpha1_access_logging_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
