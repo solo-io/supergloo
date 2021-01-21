a05a56c11555a2ac515b7e8b54bf81e7d413a52d
EItanya <eitan.yarmush@solo.io>
2021-01-21 11:35:39 -0500

    readd thing

--- pkg/mesh-discovery/reconciliation/discovery_reconciler.go
+++ pkg/mesh-discovery/reconciliation/discovery_reconciler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	settingsv1alpha2 "github.com/solo-io/gloo-mesh/pkg/api/settings.mesh.gloo.solo.io/v1alpha2"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
@@ -85,6 +86,18 @@ func Start(
 
 	filterDiscoveryEvents := skpredicate.SimplePredicate{
 		Filter: skpredicate.SimpleEventFilterFunc(isLeaderElectionObject),
+	}
+
+	// Needed in order to use field selector on metadata.name for Settings CRD.
+	if err := localMgr.GetFieldIndexer().IndexField(
+		ctx,
+		&settingsv1alpha2.Settings{},
+		"metadata.name",
+		func(object client.Object) []string {
+			settings := object.(*settingsv1alpha2.Settings)
+			return []string{settings.Name}
+		}); err != nil {
+		return err
 	}
 
 	if clusters != nil {
