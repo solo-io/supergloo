5f66c39cde07ca265923993b95c077c616040c2c
Joe Kelley <jsphklly@gmail.com>
2021-03-05 12:24:46 -0500

    install the version matching the CLI

--- pkg/meshctl/commands/install/install.go
+++ pkg/meshctl/commands/install/install.go
@@ -7,7 +7,6 @@ import (
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/commands/install/enterprise"
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/commands/install/internal/flags"
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/install/gloomesh"
-	"github.com/solo-io/gloo-mesh/pkg/meshctl/install/helm"
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/registration"
 	"github.com/solo-io/gloo-mesh/pkg/meshctl/utils"
 	"github.com/spf13/cobra"
@@ -31,17 +30,6 @@ func Command(ctx context.Context, globalFlags *utils.GlobalFlags) *cobra.Command
 }
 
 func install(ctx context.Context, opts *flags.Options) error {
-	const (
-		repoURI   = "https://storage.googleapis.com/gloo-mesh"
-		chartName = "gloo-mesh"
-	)
-	if opts.Version == "" {
-		version, err := helm.GetLatestChartVersion(repoURI, chartName)
-		if err != nil {
-			return err
-		}
-		opts.Version = version
-	}
 	if err := opts.GetInstaller(gloomesh.GlooMeshChartUriTemplate).InstallGlooMesh(ctx); err != nil {
 		return eris.Wrap(err, "installing gloo-mesh")
 	}
