fdb83dd861918edd0456057a8a38aeef0af7af11
Ryan King <ryan.king@solo.io>
2021-03-23 10:08:34 -0400

    fix enterprise demo command

--- pkg/meshctl/commands/demo/common/initialize/scripts/get_relay_server_address.sh
+++ pkg/meshctl/commands/demo/common/initialize/scripts/get_relay_server_address.sh
@@ -3,6 +3,6 @@
 cluster=$0
 
 mgmtIngressAddress=$(kubectl --context kind-mgmt-cluster get node -ojson | jq -r ".items[0].status.addresses[0].address")
-mgmtIngressPort=$(kubectl --context kind-mgmt-cluster -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+mgmtIngressPort=$(kubectl --context kind-mgmt-cluster -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 
 printf ${mgmtIngressAddress}:${mgmtIngressPort}
