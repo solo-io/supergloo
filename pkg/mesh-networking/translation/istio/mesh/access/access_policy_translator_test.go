75e630288df1c65969570a78db8c585431f49fe1
EItanya <eitan.yarmush@solo.io>
2021-04-12 12:24:34 -0400

    fix access_policy test

--- pkg/mesh-networking/translation/istio/mesh/access/access_policy_translator_test.go
+++ pkg/mesh-networking/translation/istio/mesh/access/access_policy_translator_test.go
@@ -41,13 +41,17 @@ var _ = Describe("AccessPolicyTranslator", func() {
 								WorkloadLabels: map[string]string{
 									"istio": "ingressgateway",
 								},
-								ExternalAddress: "1.1.1.1",
+								ExternalAddressType: &discoveryv1.MeshSpec_Istio_IngressGatewayInfo_ExternalIp{
+									ExternalIp: "1.1.1.1",
+								},
 							},
 							{
 								WorkloadLabels: map[string]string{
 									"istio": "ingressgateway2",
 								},
-								ExternalAddress: "2.2.2.2",
+								ExternalAddressType: &discoveryv1.MeshSpec_Istio_IngressGatewayInfo_ExternalIp{
+									ExternalIp: "2.2.2.2",
+								},
 							},
 						},
 					},
