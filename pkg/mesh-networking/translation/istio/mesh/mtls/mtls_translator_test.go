0d650c84a91d7f549ab6e4a5174547b5597d17bc
EItanya <eitan.yarmush@solo.io>
2021-04-28 11:03:01 -0400

    add more logic to the tests

--- pkg/mesh-networking/translation/istio/mesh/mtls/mtls_translator_test.go
+++ pkg/mesh-networking/translation/istio/mesh/mtls/mtls_translator_test.go
@@ -8,15 +8,19 @@ import (
 
 	. "github.com/onsi/gomega"
 	v1sets "github.com/solo-io/external-apis/pkg/api/k8s/core/v1/sets"
+	certificatesv1 "github.com/solo-io/gloo-mesh/pkg/api/certificates.mesh.gloo.solo.io/v1"
 	discoveryv1 "github.com/solo-io/gloo-mesh/pkg/api/discovery.mesh.gloo.solo.io/v1"
 	mock_istio "github.com/solo-io/gloo-mesh/pkg/api/networking.mesh.gloo.solo.io/output/istio/mocks"
 	mock_local "github.com/solo-io/gloo-mesh/pkg/api/networking.mesh.gloo.solo.io/output/local/mocks"
 	networkingv1 "github.com/solo-io/gloo-mesh/pkg/api/networking.mesh.gloo.solo.io/v1"
 	mock_reporting "github.com/solo-io/gloo-mesh/pkg/mesh-networking/reporting/mocks"
 	"github.com/solo-io/gloo-mesh/pkg/mesh-networking/translation/istio/mesh/mtls"
-	"github.com/solo-io/skv2/contrib/pkg/sets"
-	v1 "github.com/solo-io/skv2/pkg/api/core.skv2.solo.io/v1"
+	"github.com/solo-io/gloo-mesh/pkg/mesh-networking/translation/utils/metautils"
+	skv2corev1 "github.com/solo-io/skv2/pkg/api/core.skv2.solo.io/v1"
+
+	// . "github.com/solo-io/skv2/test/matchers"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("MtlsTranslator", func() {
@@ -27,6 +31,9 @@ var _ = Describe("MtlsTranslator", func() {
 		mockIstioBuilder *mock_istio.MockBuilder
 		mockLocalBuilder *mock_local.MockBuilder
 		mockReporter     *mock_reporting.MockReporter
+
+		istioMesh         *discoveryv1.Mesh
+		childResourceMeta *metav1.ObjectMeta
 	)
 
 	BeforeEach(func() {
@@ -34,6 +41,35 @@ var _ = Describe("MtlsTranslator", func() {
 		mockIstioBuilder = mock_istio.NewMockBuilder(ctrl)
 		mockLocalBuilder = mock_local.NewMockBuilder(ctrl)
 		mockReporter = mock_reporting.NewMockReporter(ctrl)
+
+		istioMesh = &discoveryv1.Mesh{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "my-istio-mesh",
+			},
+			Spec: discoveryv1.MeshSpec{
+				Type: &discoveryv1.MeshSpec_Istio_{
+					Istio: &discoveryv1.MeshSpec_Istio{
+						TrustDomain:          "cluster.not-local",
+						IstiodServiceAccount: "istiod-not-standard",
+						Installation: &discoveryv1.MeshSpec_MeshInstallation{
+							Namespace: "istio-system-2",
+							Cluster:   "cluster-name",
+						},
+					},
+				},
+				AgentInfo: &discoveryv1.MeshSpec_AgentInfo{
+					AgentNamespace: "gloo-mesh",
+				},
+			},
+		}
+
+		childResourceMeta = &metav1.ObjectMeta{
+			Name:        istioMesh.GetName(),
+			Namespace:   istioMesh.Spec.GetAgentInfo().GetAgentNamespace(),
+			ClusterName: istioMesh.Spec.GetIstio().GetInstallation().GetCluster(),
+			Labels:      metautils.TranslatedObjectLabels(),
+		}
+
 	})
 
 	AfterEach(func() {
@@ -49,18 +85,14 @@ var _ = Describe("MtlsTranslator", func() {
 
 	It("generated root CA", func() {
 		certSettings := &networkingv1.VirtualMeshSpec_RootCertificateAuthority_SelfSignedCert{
-			TtlDays:         364,
-			RsaKeySizeBytes: 4097,
-			OrgName:         "my-org",
+			OrgName: "my-org",
 		}
-		mesh := &discoveryv1.Mesh{
-			Spec: discoveryv1.MeshSpec{
-				Type: &discoveryv1.MeshSpec_Istio_{
-					Istio: &discoveryv1.MeshSpec_Istio{},
-				},
-			},
-		}
+
 		vm := &discoveryv1.MeshStatus_AppliedVirtualMesh{
+			Ref: &skv2corev1.ObjectRef{
+				Name:      "my-vm",
+				Namespace: "gloo-mesh",
+			},
 			Spec: &networkingv1.VirtualMeshSpec{
 				MtlsConfig: &networkingv1.VirtualMeshSpec_MTLSConfig{
 					TrustModel: &networkingv1.VirtualMeshSpec_MTLSConfig_Shared{
@@ -79,34 +111,60 @@ var _ = Describe("MtlsTranslator", func() {
 		// Can't actually check against the exact context
 		mockLocalBuilder.EXPECT().AddSecrets(gomock.Any()).Do(func(secret *corev1.Secret) {
 			// Make sure the name is the same, maybe decode the cert and check the data?
-			Expect(sets.Key(secret)).To(Equal(vm.GetRef().GetName() + "." + vm.GetRef().GetNamespace()))
+			Expect(secret.GetName()).To(Equal(vm.GetRef().GetName() + "." + vm.GetRef().GetNamespace()))
 			Expect(mtls.IsSigningCert(secret)).To(BeTrue())
 		})
 
+		mockIstioBuilder.EXPECT().
+			AddIssuedCertificates(gomock.Any()).
+			Do(func(issuedCert *certificatesv1.IssuedCertificate) {
+				cert := &certificatesv1.IssuedCertificate{
+					ObjectMeta: *childResourceMeta,
+					Spec: certificatesv1.IssuedCertificateSpec{
+						Hosts: []string{"spiffe://cluster.not-local/ns/istio-system-2/sa/istiod-not-standard"},
+						Org:   "Istio",
+						SigningCertificateSecret: &skv2corev1.ObjectRef{
+							Name:      vm.GetRef().GetName() + "." + vm.GetRef().GetNamespace(),
+							Namespace: "gloo-mesh",
+						},
+						IssuedCertificateSecret: &skv2corev1.ObjectRef{
+							Name:      "cacerts",
+							Namespace: istioMesh.Spec.GetIstio().GetInstallation().GetNamespace(),
+						},
+					},
+				}
+				metautils.AppendParent(ctx, cert, vm.GetRef(), networkingv1.VirtualMesh{}.GVK())
+				Expect(cert).To(Equal(issuedCert))
+			})
+
+		mockIstioBuilder.EXPECT().AddPodBounceDirectives(nil)
+
 		translator := mtls.NewTranslator(ctx, v1sets.NewSecretSet(), nil)
 
-		translator.Translate(mesh, vm, mockIstioBuilder, mockLocalBuilder, mockReporter)
+		translator.Translate(istioMesh, vm, mockIstioBuilder, mockLocalBuilder, mockReporter)
 	})
 
 	It("provided root CA", func() {
 
-		generatedSecret := &corev1.Secret{}
-
-		mesh := &discoveryv1.Mesh{
-			Spec: discoveryv1.MeshSpec{
-				Type: &discoveryv1.MeshSpec_Istio_{
-					Istio: &discoveryv1.MeshSpec_Istio{},
-				},
+		generatedSecret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "my-secret",
+				Namespace: "my-namespace",
 			},
 		}
+
 		vm := &discoveryv1.MeshStatus_AppliedVirtualMesh{
+			Ref: &skv2corev1.ObjectRef{
+				Name:      "my-vm",
+				Namespace: "gloo-mesh",
+			},
 			Spec: &networkingv1.VirtualMeshSpec{
 				MtlsConfig: &networkingv1.VirtualMeshSpec_MTLSConfig{
 					TrustModel: &networkingv1.VirtualMeshSpec_MTLSConfig_Shared{
 						Shared: &networkingv1.VirtualMeshSpec_MTLSConfig_SharedTrust{
 							RootCertificateAuthority: &networkingv1.VirtualMeshSpec_RootCertificateAuthority{
 								CaSource: &networkingv1.VirtualMeshSpec_RootCertificateAuthority_Secret{
-									Secret: &v1.ObjectRef{
+									Secret: &skv2corev1.ObjectRef{
 										Name:      generatedSecret.GetName(),
 										Namespace: generatedSecret.GetNamespace(),
 									},
@@ -118,9 +176,33 @@ var _ = Describe("MtlsTranslator", func() {
 			},
 		}
 
+		mockIstioBuilder.EXPECT().
+			AddIssuedCertificates(gomock.Any()).
+			Do(func(issuedCert *certificatesv1.IssuedCertificate) {
+				cert := &certificatesv1.IssuedCertificate{
+					ObjectMeta: *childResourceMeta,
+					Spec: certificatesv1.IssuedCertificateSpec{
+						Hosts: []string{"spiffe://cluster.not-local/ns/istio-system-2/sa/istiod-not-standard"},
+						Org:   "Istio",
+						SigningCertificateSecret: &skv2corev1.ObjectRef{
+							Name:      generatedSecret.GetName(),
+							Namespace: generatedSecret.GetNamespace(),
+						},
+						IssuedCertificateSecret: &skv2corev1.ObjectRef{
+							Name:      "cacerts",
+							Namespace: istioMesh.Spec.GetIstio().GetInstallation().GetNamespace(),
+						},
+					},
+				}
+				metautils.AppendParent(ctx, cert, vm.GetRef(), networkingv1.VirtualMesh{}.GVK())
+				Expect(cert).To(Equal(issuedCert))
+			})
+
+		mockIstioBuilder.EXPECT().AddPodBounceDirectives(nil)
+
 		translator := mtls.NewTranslator(ctx, v1sets.NewSecretSet(generatedSecret), nil)
 
-		translator.Translate(mesh, vm, mockIstioBuilder, mockLocalBuilder, mockReporter)
+		translator.Translate(istioMesh, vm, mockIstioBuilder, mockLocalBuilder, mockReporter)
 	})
 
 })
