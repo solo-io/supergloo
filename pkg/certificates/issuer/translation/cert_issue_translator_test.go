1046334db67069035e52b0470aeb59d573c9de76
EItanya <eitan.yarmush@solo.io>
2021-05-10 20:56:10 -0400

    fix test

--- pkg/certificates/issuer/translation/cert_issue_translator_test.go
+++ pkg/certificates/issuer/translation/cert_issue_translator_test.go
@@ -35,7 +35,7 @@ var _ = Describe("CertIssueTranslator", func() {
 		ctrl.Finish()
 	})
 
-	XIt("will do nothing if no signing cert is present", func() {
+	It("will do nothing if no signing cert is present", func() {
 
 		translator := translation.NewTranslator(mockSecretClient)
 
