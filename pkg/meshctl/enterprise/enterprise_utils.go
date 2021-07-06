4479af85b8ef1487618da871efda25a1d2307b0e
Yuval Kohavi <yuval.kohavi@gmail.com>
2021-07-06 16:06:34 -0400

    no need for old option

--- pkg/meshctl/enterprise/enterprise_utils.go
+++ pkg/meshctl/enterprise/enterprise_utils.go
@@ -169,7 +169,6 @@ func RegisterCluster(ctx context.Context, opts RegistrationOptions) error {
 	values := map[string]string{
 		"relay.serverAddress": opts.RelayServerAddress,
 		"relay.authority":     "enterprise-networking.gloo-mesh",
-		"relay.insecure":      strconv.FormatBool(opts.RelayServerInsecure),
 		"global.insecure":     strconv.FormatBool(opts.RelayServerInsecure),
 		"relay.cluster":       opts.ClusterName,
 	}
